07ae62dcb84fd6a4aa12b93592e976c7997e79b0
Kamil <kamil.figon@gmail.com>
2020-12-28 12:26:28 +0100

    minor code refactoring

--- web-server-project/phigo/main.go
+++ web-server-project/phigo/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"net"
 	"log"
+	"phigo/core"
 )
 
 func listner()  {
@@ -14,47 +14,6 @@ func listner()  {
 	defer conn.Close()
 }
 
-func createServer()  {
-	ln, err := net.Listen("tcp", "localhost:8080")
-	if err != nil {
-		log.Fatal("can't create server", err)
-	}
-	defer ln.Close()
-
-	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			log.Println("error during listening to requests", err)
-			continue
-		}
-		defer conn.Close()
-
-		go handleConnection(conn)
-	}
-}
-
-func handleConnection(conn net.Conn)  {
-	var data = make([]byte, 512)
-	byteLen, err := conn.Read(data)
-	if err != nil {
-		log.Println("error when reading data", err)
-	}
-	log.Println("response size", byteLen)
-	fmt.Println(string(data))
-
-	responseBody := fmt.Sprintf("HTTP/1.1 200 OK\r\n"+
-					"Server: Phigo\r\n"+
-					"Date: Mon, 28 Dec 2020 11:48:50 CET\r\n"+
-					"Content-Type: text/html\r\n"+
-					"Content-Length: 0\r\n\r\n")
-
-	byteLen, err = conn.Write([]byte(responseBody))
-	if err != nil {
-		log.Println("error during responding", err)
-	}
-	log.Println("sent response with bytes:", byteLen)
-}
-
 func main() {
-	createServer()
+	core.NewServer(8080).Run()
 }	
