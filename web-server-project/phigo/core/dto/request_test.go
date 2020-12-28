8db9991c6cf79929299da88ed7063a6bb5daf7e4
Kamil <kamil.figon@gmail.com>
2020-12-28 21:17:45 +0100

    table driven test added

--- web-server-project/phigo/core/dto/request_test.go
+++ web-server-project/phigo/core/dto/request_test.go
@@ -1,6 +1,9 @@
 package dto
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestParseGet(t *testing.T) {
 	body:=`GET / HTTP/1.1\r\nHost: localhost:8080\r\nUser-Agent: curl/7.68.0\r\nAccept: */*\r\n\r\n\r\n`
@@ -19,36 +22,19 @@ func TestParseGet(t *testing.T) {
 	}
 }
 
-func TestEmptyBody(t *testing.T) {
-	_, err := ParseResponse([]byte(``))
-	if err == nil {
-		t.Error("Error should be present")
-	}
-}
-
 func TestInvalidBody(t *testing.T) {
-	_, err := ParseResponse([]byte(` `))
-	if err == nil {
-		t.Error("Error should be present when blank data")
+	testCases := []struct {in string} {
+		{in: ` `},
+		{in: ``},
+		{in: `GET asd`},
+		{in: `GET / HTTP/1.0`},
 	}
 
-	_, err = ParseResponse([]byte(``))
-	if err == nil {
-		t.Error("Error should be present when empty data")
-	}
-}
-
-func TestInvalidBody2(t *testing.T) {
-	_, err := ParseResponse([]byte(`GET asd`))
-	if err == nil {
-		t.Error("Error should be present")
-	}
-}
-func TestInvalidFirstLine(t *testing.T) {
-	body:=`GET / HTTP/1.0`
-
-	_, err := ParseResponse([]byte(body))
-	if err == nil {
-		t.Error("Error should be present")
+	for _,testCase := range testCases {
+		t.Run(fmt.Sprintf("%q", testCase.in), func(t *testing.T) {
+			if _, err := ParseResponse([]byte(testCase.in)); err == nil {
+				t.Error("Error should be present when blank data")
+			}
+		})
 	}
 }
