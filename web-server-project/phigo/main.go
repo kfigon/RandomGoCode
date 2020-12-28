c1b7e8f77358e0ab7005dca03e65f747aec0663b
Kamil <kamil.figon@gmail.com>
2020-12-28 12:02:03 +0100

    basic socket communication

--- web-server-project/phigo/main.go
+++ web-server-project/phigo/main.go
@@ -2,10 +2,56 @@ package main
 
 import (
 	"fmt"
-	"phigo/core"
+	"net"
+	"log"
+	// "time"
 )
 
-func main()  {
-	fmt.Println("hello world")
-	fmt.Println("stuff from core lib:", core.Hello())
+func listner()  {
+	conn, err := net.Dial("tcp", "localhost:80")
+	if err != nil {
+		log.Fatal("error during opening connection", err)
+	}
+	defer conn.Close()
 }
+
+func createServer()  {
+	
+	ln, err := net.Listen("tcp", "localhost:8080")
+	
+	if err != nil {
+		log.Fatal("can't create server", err)
+	}
+	defer ln.Close()
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Println("error during listening to requests", err)
+			continue
+		}
+		var data = make([]byte, 512)
+		byteLen, err := conn.Read(data)
+		if err != nil {
+			log.Println("error when reading data", err)
+		}
+		log.Println("response size", byteLen)
+		fmt.Println(string(data))
+
+		responseBody := fmt.Sprintf("HTTP/1.1 200 OK\r\n"+
+						"Server: Phigo\r\n"+
+						"Date: Mon, 28 Dec 2020 11:48:50 CET\r\n"+
+						"Content-Type: text/html\r\n"+
+						"Content-Length: 0\r\n")
+
+		byteLen, err = conn.Write([]byte(responseBody))
+		if err != nil {
+			log.Println("error during responding", err)
+		}
+		log.Println("sent response with bytes:", byteLen)
+	}
+}
+
+func main() {
+	createServer()
+}	
