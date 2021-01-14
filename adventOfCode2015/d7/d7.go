13d17df841fbab1fbfd3841ed5e0447fd6cafe1a
Kamil <kamil.figon@gmail.com>
2021-01-14 22:08:37 +0100

    parser done

--- adventOfCode2015/d7/d7.go
+++ adventOfCode2015/d7/d7.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"strconv"
-	"strings"
 	"fmt"
 	"regexp"
 )
@@ -69,16 +68,85 @@ func parseLine(input string) (operation, error) {
 		return operation{}, fmt.Errorf("Empty input provided")
 	}
 
-	twoArgOperation := parsePattern(input, `(\w+)?(\d+)? (\w+) (\w+)?(\d+)? -> (\w+)`)
-	if len(twoArgOperation) != 0 {
-		
+	if op, isOk := parseTwoArgCommand(input); isOk {
+		return op, nil
+	} else if op, isOk := parseLoad(input); isOk {
+		return op, nil
+	} else if op, isOk := parseNot(input); isOk {
+		return op, nil
 	}
 
-	// targetRegister := parts[1]
-	// firstPart := parts[0]
-	
+	return operation{}, fmt.Errorf("invalid input %q", input)
+}
 
-	return operation{}, nil
+func parseTwoArgCommand(input string) (operation, bool) {
+	twoArgOperation := parsePattern(input, `(\w+)?(\d+)? (\w+) (\w+)?(\d+)? -> (\w+)`)
+	if twoArgOperation == nil || len(twoArgOperation) != 6 {
+		return operation{}, false
+	}
+
+	parseOp := func(x string) int {
+		switch x {
+		case "AND": return and
+		case "OR": return or
+		case "LSHIFT": return lshift
+		case "RSHIFT": return rshift
+		}
+		return -1
+	}
+
+	opCode := parseOp(twoArgOperation[2])
+	if opCode == -1 {
+		return operation{}, false
+	}
+
+	arg1 := parseInt(twoArgOperation[1])
+	arg2 := parseInt(twoArgOperation[4])
+	sourceReg1 := twoArgOperation[0]
+	sourceReg2 := twoArgOperation[3]
+	destReg := twoArgOperation[5]
+
+	return operation{
+		arg1: arg1,
+		arg2: arg2,
+		operation: opCode,
+		sourceReg1: sourceReg1,
+		sourceReg2: sourceReg2,
+		destinationReg: destReg,
+	}, true
+}
+
+func parseInt(x string) uint16 {
+	v, err := strconv.Atoi(x)
+	if err != nil {
+		return 0
+	}
+	return uint16(v)
+}
+
+func parseLoad(input string) (operation, bool) {
+	loadOperation := parsePattern(input, `(\d+) -> (\w+)`)
+	if loadOperation == nil || len(loadOperation) != 2 {
+		return operation{}, false
+	}
+
+	return operation{
+		operation: not,
+		arg1: parseInt(loadOperation[0]),
+		destinationReg: loadOperation[1],
+	}, true
+}
+
+func parseNot(input string) (operation, bool) {
+	notOperation := parsePattern(input, `NOT (\w+) -> (\w+)`)
+	if notOperation == nil || len(notOperation) != 2 {
+		return operation{}, false
+	}
+
+	return operation{
+		sourceReg1: notOperation[0],
+		destinationReg: notOperation[1],
+	}, true
 }
 
 func parsePattern(input string, pattern string) ([]string) {
