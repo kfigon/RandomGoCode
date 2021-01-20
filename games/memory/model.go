e2af1cd0577e61a0e65036093d5848e9c830c5bf
Kamil <kamil.figon@gmail.com>
2021-01-20 20:03:31 +0100

    shuffling done

--- games/memory/model.go
+++ games/memory/model.go
@@ -2,6 +2,7 @@ package memory
 
 import (
 	"math/rand"
+	"time"
 )
 
 type field struct {
@@ -17,7 +18,27 @@ const numberOfFields = 16
 
 
 func generatePairsAndShuffle(numOfGameFields int) []int {
-	
+	rand.Seed(time.Now().UnixNano())
+	vals := make([]int,numOfGameFields)
+	counter := 0
+
+	for i := 0; i < len(vals); i+=2 {
+		vals[i] = counter
+		vals[i+1]= counter
+		counter++
+	}
+
+	// shuffle
+	for i := 0; i < len(vals); i++ {
+		src := rand.Intn(len(vals))
+		dst := rand.Intn(len(vals))
+		
+		tmp := vals[src]
+		vals[src] = vals[dst]
+		vals[dst] = tmp
+	}
+
+	return vals
 }
 
 func newField() *game {
@@ -25,11 +46,11 @@ func newField() *game {
 	fieldValues := generatePairsAndShuffle(numberOfFields)
 
 	for i := 0; i < len(fields); i++ {
-		fields[i] = field{revealed: false, val: fieldValues[i]}
+		fields[i] = field {revealed: false, val: fieldValues[i]}
 	}
 
 	return &game{
-		fields: fields
+		fields: fields,
 	}
 }
 
--- games/memory/model_test.go
+++ games/memory/model_test.go
@@ -2,18 +2,35 @@ package memory
 
 import (
 	"testing"
-	"fmt"
+	// "fmt"
 )
 
 func TestInitGame(t *testing.T) {
 	game := newField()
 	for i := 0; i < game.size(); i++ {
 		if game.isRevealed(i) {
-			t.Errorf("Error %v shouldn't be revealed at the start")
+			t.Errorf("Field %v shouldn't be revealed at the start", i)
 		}
 	}
 }
 
 func TestGenerateAndShuffle(t *testing.T)  {
-	
+	maxValue := 8
+	vals := generatePairsAndShuffle(maxValue)
+	if len(vals) != 8 {
+		t.Fatal("Wrong number of values", len(vals))
+	}
+
+	occurences := make(map[int]int)
+	for _,v := range vals {
+		occurences[v]++
+	}
+	if len(occurences) != 4 {
+		t.Fatalf("wrong number of unique values: %v", len(occurences))
+	}
+	for key := range occurences {
+		if occurences[key] != 2 {
+			t.Errorf("Key %q, has invalid occurences %v", key, occurences[key])
+		}
+	}
 }
