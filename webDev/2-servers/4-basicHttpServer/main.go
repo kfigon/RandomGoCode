0f51d369b589264bac038a19c3482cad306bb662
Kamil <kamil.figon@gmail.com>
2021-01-23 18:22:41 +0100

    basic http

--- webDev/2-servers/4-basicHttpServer/main.go
+++ webDev/2-servers/4-basicHttpServer/main.go
@@ -21,26 +21,36 @@ func main() {
 }
 
 func handle(conn net.Conn)  {
-	parseReq(conn)
-	response(conn, `<html>hi!</html>`)
+	req := parseReq(conn)
+	response(conn, req)
 	conn.Close()
 }
 
-func parseReq(conn net.Conn) {
+type req struct {
+	method string
+	url string
+	headers map[string]string
+	body []string
+}
+
+func parseReq(conn net.Conn) *req {
 	log.Println("==============New req:")
 	scan := bufio.NewScanner(conn)
 	i := 0
+	var r req
+	r.headers = make(map[string]string)
 	for scan.Scan() {
 		line := scan.Text()
 		if line == "" {
 			break
 		}
-		parse(line, i)
+		parse(line, i, &r)
 		i++
 	}
+	return &r
 }
 
-func parse(line string, lineNum int)  {
+func parse(line string, lineNum int, r *req)  {
 	if lineNum == 0 {
 		elements := strings.Fields(line)
 		if len(elements) != 3 {
@@ -48,7 +58,9 @@ func parse(line string, lineNum int)  {
 			return
 		}
 		log.Printf("Method: %q, URI: %q, protocolVer: %q\n", elements[0], elements[1], elements[2])
-		return
+		r.method = elements[0]
+		r.url = elements[1]
+		return 
 	}
 
 	h := strings.Split(line, ": ")
@@ -57,9 +69,13 @@ func parse(line string, lineNum int)  {
 		return
 	}
 	log.Printf("Header: %q : %q'n", h[0], h[1])
+	r.headers[h[0]] = h[1]
 }
 
-func response(conn net.Conn, responseBody string)  {
+func response(conn net.Conn, r *req)  {
+	log.Println(r)
+
+	responseBody := `<html>hi!</html>`
 	fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\n")
 	fmt.Fprintf(conn, "Content-Length: %v\r\n", len(responseBody))
 	fmt.Fprintf(conn, "Content-Type: text/html\r\n")
