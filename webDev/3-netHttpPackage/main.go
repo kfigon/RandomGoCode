53ecc687147ff94b8feec451dc331c814d12a612
Kamil <kamil.figon@gmail.com>
2021-01-23 21:02:23 +0100

    more notes

--- webDev/3-netHttpPackage/main.go
+++ webDev/3-netHttpPackage/main.go
@@ -9,17 +9,12 @@ import (
 
 type hotdog int
 func (h hotdog) ServeHTTP(w http.ResponseWriter, r *http.Request)  {
-	path := r.URL.Path
-	if r.Method == "GET" && path == "/" {
-		getHelloTemplate(w, nil)
-	} else if r.Method == "POST" && path == "/" {
-		// providedData := r.FormValue("myName")
+	// providedData := r.FormValue("myName")
 
-		r.ParseForm()
-		values := r.Form // get collection, we can iterate in template
-		providedData := values.Get("myName")
-		getHelloTemplate(w, providedData)
-	}
+	r.ParseForm() // data in query na in form (request data)
+	values := r.Form // map[string][]string
+	providedData := values.Get("myName")
+	getHelloTemplate(w, providedData)
 }
 
 func getHelloTemplate(w io.Writer, data interface{}){
