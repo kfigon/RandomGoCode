496f92e340e11ad331f21b7a6dcaad8500c9dd4a
Kamil <kamil.figon@gmail.com>
2021-01-24 10:40:06 +0100

    base template added

--- webDev/3-netHttpPackage/main.go
+++ webDev/3-netHttpPackage/main.go
@@ -9,16 +9,18 @@ import (
 
 type hotdog int
 func (h hotdog) ServeHTTP(w http.ResponseWriter, r *http.Request)  {
+	fmt.Println("Got method", r.Method)
 	// providedData := r.FormValue("myName")
 
 	r.ParseForm() // data in query na in form (request data)
 	values := r.Form // map[string][]string
+	fmt.Println("Provided data:", values)
 	providedData := values.Get("myName")
 	getHelloTemplate(w, providedData)
 }
 
 func getHelloTemplate(w io.Writer, data interface{}){
-	tp := template.Must(template.ParseFiles("template.html"))
+	tp := template.Must(template.ParseFiles("baseTemplate.html", "template.html"))
 	tp.Execute(w, data)
 }
 
