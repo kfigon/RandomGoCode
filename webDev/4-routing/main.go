441ce20a22acc9e213caf96d00dae6c2b761276c
Kamil <kamil.figon@gmail.com>
2021-01-25 21:45:51 +0100

    cat test

--- webDev/4-routing/main.go
+++ webDev/4-routing/main.go
@@ -9,10 +9,15 @@ func helloFunc(response http.ResponseWriter, req* http.Request) {
 	io.WriteString(response, "Hello World!")
 }
 
+func cat(response http.ResponseWriter, req* http.Request) {
+	io.WriteString(response, "Hello Cat!")
+}
+
 // mux == server
 func createMux() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", helloFunc)
+	mux.HandleFunc("/cat", cat) // /cat/ - match everything after cat
 
 	return mux
 }
--- webDev/4-routing/main_test.go
+++ webDev/4-routing/main_test.go
@@ -7,24 +7,38 @@ import (
 	"testing"
 )
 
-func TestRoot(t *testing.T) {
-	server := httptest.NewServer(createMux())
-	defer server.Close()
-
-	response, err := http.Get(server.URL+"/")
+func doGet(t *testing.T, url string, expStatus int) string {
+	response, err := http.Get(url)
 	if err != nil {
 		t.Errorf("Unexpected error during request: %q", err)
 	}
 	if response.StatusCode != http.StatusOK {
 		t.Errorf("Wanted %v, got %v", http.StatusOK, response.StatusCode)
 	}
-	
+
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		t.Error("Unexpected error", err)
 	}
-	strBody := string(body)	
+	return string(body)
+}
+
+func TestRoot(t *testing.T) {
+	server := httptest.NewServer(createMux())
+	defer server.Close()
+
+	strBody := doGet(t, server.URL+"/", http.StatusOK)
 	if strBody != "Hello World!" {
+		t.Error("Invalid body: ", strBody)
+	}
+}
+
+func TestCat(t *testing.T) {
+	server := httptest.NewServer(createMux())
+	defer server.Close()
+
+	strBody := doGet(t, server.URL+"/cat", http.StatusOK)
+	if strBody != "Hello Cat!" {
 		t.Error("Invalid body: ", strBody)
 	}
 }
