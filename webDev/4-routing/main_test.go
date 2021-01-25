61fbb72a517e9ec5b597e5f7c8a20ea357a03163
Kamil <kamil.figon@gmail.com>
2021-01-25 21:01:30 +0100

    minor refactoring

--- webDev/4-routing/main_test.go
+++ webDev/4-routing/main_test.go
@@ -1,24 +1,29 @@
 package main
 
 import (
+	"net/http"
 	"testing"
 	"net/http/httptest"
 	"io/ioutil"
 )
 
-func TestRoot(t *testing.T) {
-	// request := httptest.NewRequest("GET", "http://localhost:8080/", nil)
-	response := httptest.NewRecorder()
-
+func assertStatusGetBody(t *testing.T, expStatus int, response *httptest.ResponseRecorder) string {
 	status := response.Result().StatusCode
-	if status != 200 {
-		t.Error("Wanted 200, got", status)
+	if status != expStatus {
+		t.Errorf("Wanted %v, got %v", expStatus, status)
 	}
 	body, err := ioutil.ReadAll(response.Result().Body)
 	if err != nil {
 		t.Error("Unexpected error", err)
 	}
-	strBody := string(body)
+	return string(body)
+}
+
+func TestRoot(t *testing.T) {
+	// request := httptest.NewRequest("GET", "http://localhost:8080/", nil)
+	response := httptest.NewRecorder()
+
+	strBody := assertStatusGetBody(t, http.StatusOK, response)
 	if strBody != "Hello World" {
 		t.Error("Invalid body", strBody)
 	}
