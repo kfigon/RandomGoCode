bc6f4736d00830fa95aa756574898e36e2e100ee
Kamil <kamil.figon@gmail.com>
2021-01-26 19:44:55 +0100

    serving image

--- webDev/5-servingFiles/main.go
+++ webDev/5-servingFiles/main.go
@@ -9,9 +9,15 @@ func greet(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hi!")
 }
 
+func servePicture(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, `<!DOCTYPE html><html><body><img src="pic.jpg"></body></html>`)
+}
+
 func createMux() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", greet)
+	mux.HandleFunc("/picture", servePicture)
 	return mux
 }
 
--- webDev/5-servingFiles/main_test.go
+++ webDev/5-servingFiles/main_test.go
@@ -5,6 +5,7 @@ import(
 	"net/http/httptest"
 	"net/http"
 	"io/ioutil"
+	"strings"
 )
 
 func TestHello(t *testing.T) {
@@ -16,12 +17,36 @@ func TestHello(t *testing.T) {
 		t.Fatal("Got error during request: ", err)
 	}
 
+	strBody := readBodyAsString(t, response)
+	if strBody != "Hi!" {
+		t.Errorf("Invalid body. Wanted %q, got %q", "Hi!", strBody)
+	}
+}
+
+func readBodyAsString(t *testing.T, response *http.Response) string{
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		t.Fatal("Got error when reading response: ", err)
 	}
-	strBody := string(body)
-	if strBody != "Hi!" {
-		t.Errorf("Invalid body. Wanted %q, got %q", "Hi!", strBody)
+	return string(body)
+}
+
+func TestImage(t *testing.T) {
+	server := httptest.NewServer(createMux())
+	defer server.Close()
+
+	response, err := http.Get(server.URL+"/picture")
+	if err != nil {
+		t.Fatal("Got error during request: ", err)
+	}
+
+	header := response.Header.Get("Content-Type")
+	if header != "text/html; charset=utf-8" {
+		t.Error("Got invalid header: ", header)
+	}
+
+	responseString := readBodyAsString(t, response)
+	if !strings.Contains(responseString, `<img src="pic.jpg">`) {
+		t.Error("not contains img tag: ", responseString)
 	}
 }
