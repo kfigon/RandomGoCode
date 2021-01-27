815b7748a55c24ee1b08a864a6df79e2989c525d
Kamil <kamil.figon@gmail.com>
2021-01-27 23:40:22 +0100

    upload file

--- webDev/6-uploadFile/main.go
+++ webDev/6-uploadFile/main.go
@@ -1,18 +1,29 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
 	"log"
-	"io"
+
 )
 
-func greet(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello!")
+func upload(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.NotFound(w, r)
+		return
+	}
+
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil || len(data) == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.WriteHeader(http.StatusAccepted)
 }
 
 func createMux() *http.ServeMux {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", greet)
+	mux.HandleFunc("/upload", upload)
 
 	return mux
 }
--- webDev/6-uploadFile/main_test.go
+++ webDev/6-uploadFile/main_test.go
@@ -4,14 +4,48 @@ import (
 	"testing"
 	"net/http/httptest"
 	"net/http"
+	"strings"
 )
 
-func TestHello(t *testing.T) {
+func createServer() (string, func()) {
 	srv := httptest.NewServer(createMux())
-	defer srv.Close()
+	return srv.URL, srv.Close
+}
 
-	response, _ := http.Get(srv.URL+"/")
-	if response.StatusCode != http.StatusOK {
+func TestHello(t *testing.T) {
+	baseURL, closeFun := createServer()
+	defer closeFun()
+
+	response, _ := http.Get(baseURL+"/upload")
+	if response.StatusCode != http.StatusNotFound {
 		t.Errorf("Invalid status, got: %v", response.StatusCode)
+	}
+}
+
+func TestUploadFile(t *testing.T) {
+	baseURL, closeFun := createServer()
+	defer closeFun()
+
+	fileData := strings.NewReader("this is my file!")
+	response, _ := http.Post(baseURL+"/upload", "multipart/form-data", fileData)
+	
+	gotStatus := response.StatusCode
+	wantedStatus := http.StatusAccepted
+	if gotStatus != wantedStatus {
+		t.Errorf("Invalid status, got: %v, want: %v", gotStatus, wantedStatus)
+	}
+}
+
+func TestUploadEmptyFile(t *testing.T) {
+	baseURL, closeFun := createServer()
+	defer closeFun()
+
+	fileData := strings.NewReader("")
+	response, _ := http.Post(baseURL+"/upload", "multipart/form-data", fileData)
+	
+	gotStatus := response.StatusCode
+	wantedStatus := http.StatusBadRequest
+	if gotStatus != wantedStatus {
+		t.Errorf("Invalid status, got: %v, want: %v", gotStatus, wantedStatus)
 	}
 }
