6c17bd6b06f3f1cdb50cb56d4da69d8f14292053
Kamil <kamil.figon@gmail.com>
2021-01-29 15:40:55 +0100

    notes

--- webDev/8-cookies/main.go
+++ webDev/8-cookies/main.go
@@ -5,6 +5,10 @@ import (
 	"net/http"
 )
 
+// cookie - small file that server can write to clients machine!
+// used for adding state to http (session)
+// cookies are per domain
+// browser checks if we got cookie for that domain and just sends that every time, on every request
 func greet(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hi!")
 }
