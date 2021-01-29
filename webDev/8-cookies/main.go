0e7c7d3c7a2e1c772578135470378abd35040ce5
Kamil <kamil.figon@gmail.com>
2021-01-29 15:56:42 +0100

    writing a cookie

--- webDev/8-cookies/main.go
+++ webDev/8-cookies/main.go
@@ -9,7 +9,28 @@ import (
 // used for adding state to http (session)
 // cookies are per domain
 // browser checks if we got cookie for that domain and just sends that every time, on every request
+
+	// Name  string
+	// Value string
+	// Path       string    // optional
+	// Domain     string    // optional
+	// Expires    time.Time // optional
+	// RawExpires string    // for reading cookies only
+	// 	// MaxAge=0 means no 'Max-Age' attribute specified.
+	// 	// MaxAge<0 means delete cookie now, equivalently 'Max-Age: 0'
+	// 	// MaxAge>0 means Max-Age attribute present and given in seconds
+	// MaxAge   int
+	// Secure   bool
+	// HttpOnly bool
+	// SameSite SameSite
+	// Raw      string
+	// Unparsed []string // Raw text of unparsed attribute-value pairs
+	
 func greet(w http.ResponseWriter, r *http.Request) {
+	http.SetCookie(w, &http.Cookie{
+		Name:"ziomCookie",
+		Value:"asd",
+	})
 	io.WriteString(w, "Hi!")
 }
 
--- webDev/8-cookies/main_test.go
+++ webDev/8-cookies/main_test.go
@@ -23,4 +23,19 @@ func TestHello(t *testing.T) {
 	if !strings.Contains(strBody, "Hi!") {
 		t.Errorf("Invalid body received: %q", strBody)
 	}
+
+	cookies := resp.Cookies()
+	if cookies == nil || len(cookies) == 0 {
+		t.Fatal("No cookies present in response")
+	}
+	if len(cookies) > 1 {
+		t.Error("Expected only 1 cookie. There are others in response: ", cookies)
+	}
+	if cookies[0].Name != "ziomCookie" {
+		t.Error("Invalid cookie received: ", cookies[0])
+	}
+	if len(cookies[0].Name) == 0 {
+		t.Error("Empty cookie received")
+	}
+
 }
