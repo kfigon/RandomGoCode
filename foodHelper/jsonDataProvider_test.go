df73dcafffebbcb7ec02992d83c6809b49edd81d
Kamil <kamil.figon@gmail.com>
2021-01-31 16:08:26 +0100

    test json added

--- foodHelper/jsonDataProvider_test.go
+++ foodHelper/jsonDataProvider_test.go
@@ -1,10 +1,41 @@
 package main
 
 import (
+	"encoding/json"
 	"testing"
 )
 
-func TestTodo(t *testing.T) {
-	t.Fail()
+// 1 - jajka
+// 2 - makaron
+// 3 - pomodory
+// 4 - mielone
+// 5 - kurczak
+// 6 - salata
+// 7 - oliwki
+var foodJSON string = `[
+    {
+        "name": "spaghetti",
+        "ingredients": [2,3,4]
+    },
+    {
+        "name": "jajecznica",
+        "ingredients": [1]
+    },
+    {
+        "name": "salatka z jajkiem",
+        "ingredients": [6,7,1]
+    },
+    {
+        "name": "salatka z kurczakiem",
+        "ingredients": [6,7,5]
+    }
+]`
+
+var ingredientsJSON string = `[1,2,3,4,5,6,7]`
+
+func TestReadFoodbase(t *testing.T) {
+	data := `[
+		{}
+		]`
 }
 
