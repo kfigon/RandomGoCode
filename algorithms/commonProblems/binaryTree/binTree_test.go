fade4b2eef0e7a878a95d2b757031d6287e1ad58
Kamil <kamil.figon@gmail.com>
2021-02-04 20:16:10 +0100

    more tests

--- algorithms/commonProblems/binaryTree/binTree_test.go
+++ algorithms/commonProblems/binaryTree/binTree_test.go
@@ -4,18 +4,13 @@ import (
 	"testing"
 )
 
-func TestEmpty(t *testing.T) {
-	tr := newTree()
-	assertElements(t, tr, []int{})
-}
-
 func assertElements(t *testing.T, tr *tree, exp []int) {
 	if v := tr.size(); v != len(exp) {
 		t.Errorf("Invalid size, exp: %v got: %v", len(exp), v)
 	}
 	vals := tr.values()
 	if ln := len(vals); ln != len(exp) {
-		t.Errorf("Invalid size on values, exp: %v got: %v", len(exp), len(vals))
+		t.Fatalf("Invalid size on values, exp: %v got: %v", len(exp), len(vals))
 	}
 	for i := range exp {
 		e := exp[i]
@@ -26,9 +21,26 @@ func assertElements(t *testing.T, tr *tree, exp []int) {
 	}
 }
 
-func TestOneElement(t *testing.T) {
-	tr := newTree()
-	tr.insert(5)
-	assertElements(t, tr, []int{5})
+func TestInserts(t *testing.T) {
+	testCases := []struct {
+		desc string
+		in []int
+		exp []int		
+	}{
+		{"empty", []int{}, []int{}},
+		{"single elem", []int{5}, []int{5}},
+		{"elements in order", []int{1,2,3,4,5}, []int{1,2,3,4,5}},
+		{"elements in not bad order", []int{5,4,3,2,1,}, []int{1,2,3,4,5}},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			tr := newTree()
+			for _,v := range tc.in {
+				tr.insert(v)
+			}
+
+			assertElements(t, tr, tc.exp)
+		})
+	}
 }
 
