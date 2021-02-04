3451628b5a8a94638d97c6c660449627c551965f
Kamil <kamil.figon@gmail.com>
2021-02-04 20:08:14 +0100

    first step

--- algorithms/commonProblems/binaryTree/binTree.go
+++ algorithms/commonProblems/binaryTree/binTree.go
@@ -1,19 +1,38 @@
 package main
 
-type tree struct {}
+type tree struct {
+	top *node
+}
 
 func (t *tree) size() int {
-	return 0
+	return len(t.values())
 }
 
 func (t *tree) values() []int {
-	return []int{}
+	out := make([]int,0)
+	if t.top != nil {
+		out = append(out, t.top.val)
+	}
+	return out
 }
 
 func (t *tree) insert(val int) {
-	
+	if t.top == nil {
+		t.top = newNode(val)
+		return
+	}
 }
 
 func newTree() *tree {
 	return &tree{}
+}
+
+type node struct {
+	val int
+	left *node
+	right *node
+}
+
+func newNode(val int) *node {
+	return &node{val:val}
 }
