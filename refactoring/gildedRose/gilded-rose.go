74608a091df3b6aa69ba12f6b28449442f0ac132
Kamil <kamil.figon@gmail.com>
2021-02-07 17:55:09 +0100

    Squashed commit of the following:
    commit 70699d4a38e9888d4b6efd98f0faefc8e2a4a9df
    Author: Kamil <kamil.figon@gmail.com>
    Date:   Sun Feb 7 17:54:07 2021 +0100
    
        nice refactoring man!
    
    commit 033f7401cb9bd41edac613bd6abe820dd4e16156
    Author: Kamil <kamil.figon@gmail.com>
    Date:   Sun Feb 7 17:48:26 2021 +0100
    
        cleanup
    
    commit b76f662ef6a88bec4ca9c6e7654a402716b0a1c5
    Author: Kamil <kamil.figon@gmail.com>
    Date:   Sun Feb 7 17:42:59 2021 +0100
    
        abstractions added
    
    commit aebe20ec413ebed3f9c16afcc4cc411b143fdab2
    Author: Kamil <kamil.figon@gmail.com>
    Date:   Sun Feb 7 17:37:28 2021 +0100
    
        pending refactoring
    
    commit 5980a2876ec8b1141f1c8d2d2607f88eac63416f
    Author: Kamil <kamil.figon@gmail.com>
    Date:   Sun Feb 7 17:31:57 2021 +0100
    
        pending abstraction
    
    commit f2d5df7531f2cedab5ca9c1ba6a10fc66ddb0052
    Author: Kamil <kamil.figon@gmail.com>
    Date:   Sun Feb 7 17:19:02 2021 +0100
    
        small refactorings
    
    commit 35dbb3791731fbffbb18d5d5657706ebbb223b3b
    Author: Kamil <kamil.figon@gmail.com>
    Date:   Sun Feb 7 17:16:03 2021 +0100
    
        ifs rearranged

--- refactoring/gildedRose/gilded-rose.go
+++ refactoring/gildedRose/gilded-rose.go
@@ -5,52 +5,13 @@ type Item struct {
 	sellIn, quality int
 }
 
-
 func processSingleItem(item *Item) {
-	if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" {
-		if item.quality > 0 {
-			if item.name != "Sulfuras, Hand of Ragnaros" {
-				item.quality = item.quality - 1
-			}
-		}
-	} else {
-		if item.quality < 50 {
-			item.quality = item.quality + 1
-			if item.name == "Backstage passes to a TAFKAL80ETC concert" {
-				if item.sellIn < 11 {
-					if item.quality < 50 {
-						item.quality = item.quality + 1
-					}
-				}
-				if item.sellIn < 6 {
-					if item.quality < 50 {
-						item.quality = item.quality + 1
-					}
-				}
-			}
-		}
-	}
+	wrapper := createWrapper(item.name)
 
-	if item.name != "Sulfuras, Hand of Ragnaros" {
-		item.sellIn = item.sellIn - 1
-	}
-
+	wrapper.updateQuality(item)
+	wrapper.updateSellIn(item)
 	if item.sellIn < 0 {
-		if item.name != "Aged Brie" {
-			if item.name != "Backstage passes to a TAFKAL80ETC concert" {
-				if item.quality > 0 {
-					if item.name != "Sulfuras, Hand of Ragnaros" {
-						item.quality = item.quality - 1
-					}
-				}
-			} else {
-				item.quality = item.quality - item.quality
-			}
-		} else {
-			if item.quality < 50 {
-				item.quality = item.quality + 1
-			}
-		}
+		wrapper.updateQualityAfterSellinPass(item)
 	}
 }
 
@@ -58,4 +19,100 @@ func UpdateQuality(items []*Item) {
 	for i := 0; i < len(items); i++ {
 		processSingleItem(items[i])
 	}
+}
+
+func createWrapper(name string) itemWrapper {
+	switch name {
+	case "Aged Brie": return agedBrieItem{}
+	case "Backstage passes to a TAFKAL80ETC concert": return backstagePassItem{}
+	case "Sulfuras, Hand of Ragnaros": return sulfurasItem{}
+	default: return regularItem{}
+	}
+
+}
+
+func updateSellIn(item *Item) {
+	item.sellIn = item.sellIn - 1
+}
+
+func increaseQuality(item *Item) {
+	if item.quality < 50 {
+		item.quality = item.quality + 1
+	}
+}
+
+func decreaseQuality(item *Item) {
+	if item.quality > 0 {
+		item.quality = item.quality - 1
+	}
+}
+
+type itemWrapper interface {
+	updateQuality(item *Item)
+	updateSellIn(item *Item)
+	updateQualityAfterSellinPass(item *Item)
+}
+
+type regularItem struct{}
+func (r regularItem) updateQuality(item *Item) {
+	decreaseQuality(item)
+}
+
+func (r regularItem) updateSellIn(item *Item) {
+	updateSellIn(item)
+}
+
+func (r regularItem) updateQualityAfterSellinPass(item *Item) {
+	decreaseQuality(item)
+}
+
+type agedBrieItem struct{}
+func (a agedBrieItem) updateQuality(item *Item) {
+	increaseQuality(item)
+}
+
+func (a agedBrieItem) updateSellIn(item *Item) {
+	updateSellIn(item)
+}
+
+func (a agedBrieItem) updateQualityAfterSellinPass(item *Item) {
+	increaseQuality(item)
+}
+
+type backstagePassItem struct{}
+func (b backstagePassItem) updateQuality(item *Item) {
+	howManyUpdates := b.howManyQualityIncreases(item.sellIn)
+	for i := 0; i < howManyUpdates; i++ {
+		increaseQuality(item)
+	}
+}
+
+func (b backstagePassItem) howManyQualityIncreases(sellIn int) int {
+	if sellIn < 6 {
+		return 3
+	} else if sellIn < 11 {
+		return 2
+	}
+	return 1
+}
+
+func (b backstagePassItem) updateSellIn(item *Item) {
+	updateSellIn(item)
+}
+
+func (b backstagePassItem) updateQualityAfterSellinPass(item *Item) {
+	item.quality = 0
+}
+
+type sulfurasItem struct{}
+func (s sulfurasItem) updateQuality(item *Item) {
+	increaseQuality(item)
+}
+
+func (s sulfurasItem) updateSellIn(item *Item) {
+	return // do nothing
+}
+
+func (s sulfurasItem) updateQualityAfterSellinPass(item *Item) {
+	return // do nothing
 }
