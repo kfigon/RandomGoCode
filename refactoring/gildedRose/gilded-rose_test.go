9787f0baeb52e9bd5e95eb15d0631d49974cd83b
Kamil <kamil.figon@gmail.com>
2021-02-07 17:08:52 +0100

    simple test added

--- refactoring/gildedRose/gilded-rose_test.go
+++ refactoring/gildedRose/gilded-rose_test.go
@@ -1,15 +1,35 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"fmt"
 
-func Test_Foo(t *testing.T) {
-	var items = []*Item{
-		&Item{"foo", 0, 0},
+)
+
+func TestOneDay(t *testing.T) {
+	testCases := []struct {
+		in Item
+		exp Item
+	}{
+		{ Item{"+5 Dexterity Vest", 10, 20}, Item{sellIn: 9, quality: 19,}},
+		{ Item{"Aged Brie", 2, 0}, Item{sellIn: 1, quality: 1,}},
+		{ Item{"Elixir of the Mongoose", 5, 7}, Item{sellIn: 4, quality: 6,}},
+		{ Item{"Sulfuras, Hand of Ragnaros", 0, 80}, Item{sellIn: 0, quality: 80,}},
+		{ Item{"Sulfuras, Hand of Ragnaros", -1, 80}, Item{sellIn: -1, quality: 80,}},
+		{ Item{"Backstage passes to a TAFKAL80ETC concert", 15, 20}, Item{sellIn: 14, quality: 21,}},
+		{ Item{"Backstage passes to a TAFKAL80ETC concert", 10, 49}, Item{sellIn: 9, quality: 50,}},
+		{ Item{"Backstage passes to a TAFKAL80ETC concert", 5, 49}, Item{sellIn: 4, quality: 50,}},
+		{ Item{"Conjured Mana Cake", 3, 6},  Item{sellIn: 2, quality: 5,}},
 	}
-
-	UpdateQuality(items)
-
-	if items[0].name != "fixme" {
-		t.Errorf("Name: Expected %s but got %s ", "fixme", items[0].name)
+	for _, tc := range testCases {
+		t.Run(fmt.Sprint(tc.in), func(t *testing.T) {
+			processSingleItem(&tc.in)
+			if tc.exp.sellIn != tc.in.sellIn {
+				t.Errorf("%v sellIn, exp: %v, got: %v",tc.in.name, tc.exp.sellIn,  tc.in.sellIn)
+			}
+			if tc.exp.quality != tc.in.quality {
+				t.Errorf("%v quality, exp: %v, got: %v",tc.in.name, tc.exp.quality,  tc.in.quality)
+			}
+		})
 	}
 }
