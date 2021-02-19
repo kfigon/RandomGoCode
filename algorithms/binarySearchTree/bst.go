abf89f896a50d6ff2400e78a7a515ed4c1b6e0a5
Kamil <kamil.figon@gmail.com>
2021-02-19 16:53:08 +0100

    tree in progress

--- algorithms/binarySearchTree/bst.go
+++ algorithms/binarySearchTree/bst.go
@@ -3,9 +3,17 @@ package binarySearchTree
 type tree struct{}
 
 func createTree() *tree {
-	return nil
+	return &tree{}
 }
 
 func (t *tree) size() int {
-	return -1
+	return 0
+}
+
+func (t *tree) values() []int {
+	return []int{}
+}
+
+func (t *tree) insert(value int)  {
+	
 }
--- algorithms/binarySearchTree/bst_test.go
+++ algorithms/binarySearchTree/bst_test.go
@@ -2,9 +2,29 @@ package binarySearchTree
 
 import "testing"
 
+func assertSize(t *testing.T, tree *tree, expected []int) {
+	if size := tree.size(); size != len(expected) {
+		t.Errorf("Size should be %v, got %v", len(expected), size)
+	}
+	vals := tree.values()
+	if len(vals) != len(expected) {
+		t.Errorf("Value len should be %v, got %v", len(expected), len(vals))
+	}
+	for i := 0; i < len(vals); i++ {
+		exp := expected[i]
+		got := vals[i]
+		if exp != got {
+			t.Errorf("Invalid element in idx %v, got %v, exp %v", i, got, exp)
+		}
+	}
+}
 func TestCreateTree(t *testing.T) {
 	tree := createTree()
-	if tree.size() != 0 {
-		t.Error("Size should be 0, got", tree.size())
-	}
+	assertSize(t, tree, []int{})
+}
+
+func TestAdd(t *testing.T) {
+	tree := createTree()
+	tree.insert(15)
+	assertSize(t, tree, []int{15})
 }
