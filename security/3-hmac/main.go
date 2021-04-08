305dd75d9dd012243f3a982dfb5eb1e8c0835473
Kamil <kamil.figon@gmail.com>
2021-04-08 19:35:00 +0200

    pending hmac

--- security/3-hmac/main.go
+++ security/3-hmac/main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"net/http"
 	"log"
+	"crypto/hmac"
+	"crypto/sha512"
 )
 
 func main() {
@@ -18,6 +20,9 @@ type Auth struct {
 	Password string `json:"password"`
 }
 
+var secretKey = []byte("secretKey")
+var passwords = make(map[string][]byte)
+
 // curl -i -d '{ "user": "ziomx", "password": "123" }' localhost:8080
 func handleAuth(w http.ResponseWriter, r *http.Request) {
 	log.Println("got request")
@@ -29,9 +34,21 @@ func handleAuth(w http.ResponseWriter, r *http.Request) {
 	}
 
 	log.Println("Json decoded", data)
-	w.Write([]byte(`hello mr president`))
+
+	hash := hmac.New(sha512.New, secretKey)
+	_, err = hash.Write([]byte(data.Password))
+	if err != nil {
+		http.Error(w, "error in writing hash", http.StatusBadRequest)
+		return
+	}
+
+	signature := hash.Sum(nil)
+	passwords[data.User] = signature
+	log.Println("Got hash for", data.User)
+	w.Write(signature)
 }
 
+// curl -i -H "MYTOKEN: tokenValue" localhost:8080/resource
 func handleSecure(w http.ResponseWriter, r *http.Request) {
 	if !authorised(r) {
 		http.Error(w, "Unauthenticated", http.StatusForbidden)
@@ -42,5 +59,20 @@ func handleSecure(w http.ResponseWriter, r *http.Request) {
 }
 
 func authorised(r *http.Request) bool {
+	header := r.Header.Get("MYTOKEN")
+	if header == "" {
+		log.Println("No token provided")
+		return false
+	}
+	for userName := range passwords {
+		storedPass := passwords[userName]
+		ok := hmac.Equal([]byte(header), storedPass)
+		if ok {
+			log.Printf("%v authorized\n", userName)
+			return true
+		}
+	}
+
+	log.Println("Unauthorized")
 	return false
 }
