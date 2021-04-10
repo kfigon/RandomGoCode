a3ceb80d9546474630bc90b675d0594b2248dacd
Kamil <kamil.figon@gmail.com>
2021-04-10 22:24:07 +0200

    pending jwt

--- security/4-jwt/main.go
+++ security/4-jwt/main.go
@@ -1,14 +1,13 @@
 package main
 
-// JWT - praktyczne wykorzystanie HMACa do autentykacji
-
 import (
+	"fmt"
 	"net/http"
 	"log"
 	"github.com/dgrijalva/jwt-go"
 )
 
-// JWT
+// JWT - praktyczne wykorzystanie HMACa do autentykacji
 // {standard fields}.{custom fields}.{signature (hmac)}
 func main() {
 	mux := http.NewServeMux()
@@ -37,8 +36,56 @@ func handleIdx(w http.ResponseWriter, r *http.Request) {
 	}
 
 	log.Println("Login ok")
+	w.Write([]byte(createToken(`someSessionId`)))
 }
 
-func handleSecret(w http.ResponseWriter, r *http.Request) {
+type MyClaims struct {
+	jwt.Claims
+	MySessionId string
+}
 
+func createToken(data string) string {
+	c := MyClaims{
+		MySessionId: data,
+	}
+	t := jwt.NewWithClaims(jwt.SigningMethodHS512, c)
+	token, err := t.SignedString("mySecretKey")
+	if err != nil {
+		log.Println("Got error during signing", err)
+	}
+	return token
+}
+
+// curl -XGET localhost:8080/secret -I -H "MYTOKEN: eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.VFb0qJ1LRg_4ujbZoRMXnVkUgiuKq5KxWqNdbKq_G9Vvz-S1zZa9LPxtHWKa64zDl2ofkT8F6jBt_K4riU-fPg"
+
+func handleSecret(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "invalid method", http.StatusNotFound)
+		return
+	}
+	if !authenticated(r) {
+		http.Error(w, "auth error", http.StatusForbidden)
+		return
+	}
+
+	w.Write([]byte(`hello mr president`))
+}
+
+func authenticated(r *http.Request) bool {
+	receivedToken := r.Header.Get("MYTOKEN")
+	var claims *MyClaims
+	token, err := jwt.ParseWithClaims(receivedToken, claims, func(t *jwt.Token) (interface{}, error) {
+		if t.Method.Alg() != jwt.SigningMethodHS512.Alg() {
+			return nil, fmt.Errorf("invalid sign method")
+		}
+		return t, nil
+	})
+	if err != nil {
+		log.Println("Error during parseToken", err)
+		return false
+	}
+
+	log.Println("got token", token)
+	log.Println("claims", claims)
+	return token.Valid
 }
