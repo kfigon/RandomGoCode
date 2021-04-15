979cdc9ac2ea2ef75380ed341d36753c80e6bfe2
Kamil <kamil.figon@gmail.com>
2021-04-15 21:14:16 +0200

    another way of sorting

--- learning/sort/main.go
+++ learning/sort/main.go
@@ -20,14 +20,26 @@ func (a byAge) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a byAge) Less(i, j int) bool { return a[i].age < a[j].age }
 
 func main() {
-	values := []data {
+	values := createSampleValues()
+	fmt.Println(values)
+	sort.Sort(byAge(values))
+	fmt.Println(values)
+
+
+	fmt.Println("\nother method")
+	values2 := createSampleValues()
+	fmt.Println(values2)
+	sort.Slice(values2, func(i,j int) bool {
+		return values2[i].age < values2[j].age
+	})
+	fmt.Println(values2)
+}
+
+func createSampleValues() []data {
+	return []data {
 		{"asd", 1},
 		{"sad", 3},
 		{"bar", 2},
 		{"foo", 5},
 	}
-
-	fmt.Println(values)
-	sort.Sort(byAge(values))
-	fmt.Println(values)
 }
