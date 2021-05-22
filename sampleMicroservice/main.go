636eb303f8862bf0714117d1ed31d1b4b817c40b
Kamil <kamil.figon@gmail.com>
2021-05-22 18:01:29 +0200

    simple logging

--- sampleMicroservice/main.go
+++ sampleMicroservice/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"log"
+	"time"
 	"net/http"
+	"github.com/google/uuid"
 )
 
 func main() {
@@ -12,8 +14,17 @@ func main() {
 
 func newServer(loginServ *login) *http.ServeMux {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/login", loginServ.login)
-	mux.HandleFunc("/auth", loginServ.auth)
+	mux.HandleFunc("/login", withLog(loginServ.login))
+	mux.HandleFunc("/auth", withLog(loginServ.auth))
 	return mux
 }
 
+func withLog(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		uuid := uuid.New().String()
+		start := time.Now()
+		log.Printf("Starting [%v] req %v to %v\n", uuid, r.Method, r.URL)
+		next(w,r)
+		log.Printf("Ending [%v] req %v to %v, time: %v\n", uuid, r.Method, r.URL, time.Since(start))
+	}
+}
