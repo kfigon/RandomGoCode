6ef9765ea5898d09fa0fa4cfff705d5ad872ea7b
Kamil <kamil.figon@gmail.com>
2021-05-23 12:31:49 +0200

    boilerplate

--- algorithms/stanford-algorithms-1/karatsubamultiplication/karatsuba_test.go
+++ algorithms/stanford-algorithms-1/karatsubamultiplication/karatsuba_test.go
@@ -3,8 +3,30 @@ package karatsubamultiplication
 import (
 	"testing"
 	"github.com/stretchr/testify/assert"
+	"fmt"
 )
 
-func TestAsd(t *testing.T) {
-	assert.FailNow(t)
+func TestKaratsuba(t *testing.T) {
+	testCases := []struct {
+		x int
+		y int
+		exp int
+	}{
+		{1,2,2},
+		{2,3,6},
+		{3,2,6},
+		{1234,5678,7006652},
+		{5678,1234,7006652},
+	}
+	for _, tc := range testCases {
+		title := fmt.Sprintf("%v*%v", tc.x,tc.y)
+		t.Run(title, func(t *testing.T) {
+			got := karatsuba(tc.x,tc.y)
+			assert.Equal(t, tc.exp, got)
+		})
+	}
+}
+
+func karatsuba(x int, y int) int {
+	return 0
 }
