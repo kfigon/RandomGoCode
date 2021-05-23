23e246332097f8cab03f0e8a03a584472f7e4b1a
Kamil <kamil.figon@gmail.com>
2021-05-23 12:45:26 +0200

    more tests

--- algorithms/stanford-algorithms-1/karatsubamultiplication/karatsuba_test.go
+++ algorithms/stanford-algorithms-1/karatsubamultiplication/karatsuba_test.go
@@ -3,30 +3,53 @@ package karatsubamultiplication
 import (
 	"testing"
 	"github.com/stretchr/testify/assert"
-	"fmt"
 )
 
-func TestKaratsuba(t *testing.T) {
-	testCases := []struct {
-		x int
-		y int
-		exp int
-	}{
-		{1,2,2},
-		{2,3,6},
-		{3,2,6},
-		{1234,5678,7006652},
-		{5678,1234,7006652},
+type multiplicationInput struct {
+	x string
+	y string
+	exp string
+}
+
+func getInput() []multiplicationInput {
+	return []multiplicationInput{
+		{"1","2","2"},
+		{"2","3","6"},
+		{"3","2","6"},
+		{"1234","5678","7006652"},
+		{"5678","1234","7006652"},
 	}
+}
+
+func (m multiplicationInput) testName() string {
+	return m.x + "*" + m.y
+}
+
+func TestKaratsuba(t *testing.T) {
+	testCases := getInput()
 	for _, tc := range testCases {
-		title := fmt.Sprintf("%v*%v", tc.x,tc.y)
-		t.Run(title, func(t *testing.T) {
+		t.Run(tc.testName(), func(t *testing.T) {
 			got := karatsuba(tc.x,tc.y)
 			assert.Equal(t, tc.exp, got)
 		})
 	}
 }
 
-func karatsuba(x int, y int) int {
-	return 0
+
+func TestRecursive(t *testing.T) {
+	testCases := getInput()
+	for _, tc := range testCases {
+		t.Run(tc.testName(), func(t *testing.T) {
+			got := recursive(tc.x,tc.y)
+			assert.Equal(t, tc.exp, got)
+		})
+	}
+}
+
+func karatsuba(x string, y string) string {
+	return "0"
+}
+
+func recursive(x string, y string) string {
+	return "0"
 }
