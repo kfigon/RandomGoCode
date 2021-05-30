8a1beafdd17a92cdbe645db270527b5b62f469d0
Kamil <kamil.figon@gmail.com>
2021-05-30 14:04:08 +0200

    boilerplate

---
+++ musicHelper/core/note_test.go
@@ -0,0 +1,10 @@
+package core
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAsd(t *testing.T) {
+	assert.Fail(t,"todo")
+}
---
+++ musicHelper/go.mod
@@ -0,0 +1,5 @@
+module musicHelper
+
+go 1.16
+
+require github.com/stretchr/testify v1.7.0 // indirect
