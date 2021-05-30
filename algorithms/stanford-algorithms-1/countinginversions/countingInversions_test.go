9e60223b0413e59aa922c14a2dc8a9e8922f9642
Kamil <kamil.figon@gmail.com>
2021-05-30 13:47:02 +0200

    brute force applied

--- algorithms/stanford-algorithms-1/countinginversions/countingInversions_test.go
+++ algorithms/stanford-algorithms-1/countinginversions/countingInversions_test.go
@@ -6,14 +6,23 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCountInversions(t *testing.T) {
-	testCases := []struct {
-		in []int
-		exp []int
-	}{
-		{[]int{}, []int{}},
-		{[]int{1,2,3}, []int{1,2,3}},
+type testData struct {
+	in []int
+	exp int
+}
+
+func data() []testData{
+	return []testData{
+		{[]int{}, 0},
+		{[]int{1,2,3}, 0},
+		{[]int{1,3,5,2,4,6}, 3}, // 32,52,54
+		{[]int{3,2,1}, 3},
+		{[]int{4,3,2,1}, 6},
 	}
+}
+
+func TestCountInversions(t *testing.T) {
+	testCases := data()
 	for i, tc := range testCases {
 		t.Run(fmt.Sprintf("%v",i), func(t *testing.T) {
 			got := countInversions(tc.in)
@@ -22,6 +31,28 @@ func TestCountInversions(t *testing.T) {
 	}
 }
 
-func countInversions(in []int) []int {
-	return in
+func TestCountInversionsBrute(t *testing.T) {
+	testCases := data()
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("%v",i), func(t *testing.T) {
+			got := countInversionsBrute(tc.in)
+			assert.Equal(t, tc.exp, got)
+		})
+	}
+}
+
+func countInversions(in []int) int {
+	return 0
+}
+
+func countInversionsBrute(in []int) int {
+	invs := 0
+	for i := 0; i < len(in); i++ {
+		for j := 0; j < len(in); j++ {
+			if i < j && in[i] > in[j] {
+				invs += 1
+			}
+		}
+	}
+	return invs
 }
