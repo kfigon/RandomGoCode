c4d70aa08382e9e9d4960c7b0b4aec2952d89996
Kamil <kamil.figon@gmail.com>
2021-05-30 13:59:56 +0200

    pending impl

--- algorithms/stanford-algorithms-1/countinginversions/countingInversions_test.go
+++ algorithms/stanford-algorithms-1/countinginversions/countingInversions_test.go
@@ -12,12 +12,16 @@ type testData struct {
 }
 
 func data() []testData{
+	// max number - (n(n-1))/2
 	return []testData{
 		{[]int{}, 0},
 		{[]int{1,2,3}, 0},
 		{[]int{1,3,5,2,4,6}, 3}, // 32,52,54
 		{[]int{3,2,1}, 3},
 		{[]int{4,3,2,1}, 6},
+		{[]int{20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1}, 190},
+		{[]int{50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1}, 
+		1225},
 	}
 }
 
@@ -40,15 +44,23 @@ func TestCountInversionsBrute(t *testing.T) {
 		})
 	}
 }
-
+// O(nlogn)
 func countInversions(in []int) int {
+	if len(in) <= 1 {
+		return 0
+	}
 	return 0
+	// left := countInversions(in[:len(in)/2])
+	// right := countInversions(in[len(in)/2:])
+	// split := countSplitInversions(in)
+	// return left+right
 }
 
+// O(n^2)
 func countInversionsBrute(in []int) int {
 	invs := 0
 	for i := 0; i < len(in); i++ {
-		for j := 0; j < len(in); j++ {
+		for j := i; j < len(in); j++ {
 			if i < j && in[i] > in[j] {
 				invs += 1
 			}
