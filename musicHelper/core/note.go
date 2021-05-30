7766ffb643d38382b022755c42a3dca0954af212
Kamil <kamil.figon@gmail.com>
2021-05-30 14:15:20 +0200

    constants

--- musicHelper/core/note.go
+++ musicHelper/core/note.go
@@ -1,2 +1,22 @@
 package core
 
+type Note int
+
+const (
+	Ab Note = 0
+	A Note = 1
+	Bb Note = 2
+	B Note = 3
+	C Note = 4
+	Db Note = 5
+	D Note = 6
+	Eb Note = 7
+	E Note = 8
+	F Note = 9
+	Gb Note = 10
+	G Note = 11
+)
+
+func (n *Note) String() string {
+	return ""
+}
