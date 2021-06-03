d483aa09888a2e761cb4e328cbf2a7d4c146ccc0
Kamil <kamil.figon@gmail.com>
2021-06-03 14:08:05 +0200

    boiler

--- adventOfCode2019/day4/day4_test.go
+++ adventOfCode2019/day4/day4_test.go
@@ -5,11 +5,29 @@ import (
 	// "strings"
 	"testing"
 
-	// "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 	// "github.com/stretchr/testify/require"
 )
 
 // https://adventofcode.com/2019/day/4
-func TestAsd(t *testing.T) {
-	t.Fail()
+func TestPasswordCheck(t *testing.T) {
+	testCases := []struct {
+		in string	
+		exp bool
+	}{
+		{"111111", true},
+		{"223450", false},
+		{"123789", false},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.in, func(t *testing.T) {
+			got := password(tc.in).isOk()
+			assert.Equal(t, tc.exp, got)
+		})
+	}
+}
+
+type password string
+func (p password) isOk() bool {
+	return false
 }
