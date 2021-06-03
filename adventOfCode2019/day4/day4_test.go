b956439ffc751d4725e87ffc5d283a4b0fad5445
Kamil <kamil.figon@gmail.com>
2021-06-03 14:05:03 +0200

    day4

---
+++ adventOfCode2019/day4/day4_test.go
@@ -0,0 +1,15 @@
+package day4
+
+import (
+	// "strconv"
+	// "strings"
+	"testing"
+
+	// "github.com/stretchr/testify/assert"
+	// "github.com/stretchr/testify/require"
+)
+
+// https://adventofcode.com/2019/day/4
+func TestAsd(t *testing.T) {
+	t.Fail()
+}
