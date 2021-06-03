0833be581d142ff5bf0d918a23f56b9aacb05c8b
Kamil <kamil.figon@gmail.com>
2021-06-03 09:59:34 +0200

    file splitted, segment introduced

---
+++ adventOfCode2019/day3/day3.go
@@ -0,0 +1,52 @@
+package day3
+
+import (
+	"strconv"
+	"strings"
+)
+
+type point struct {
+	x int
+	y int
+}
+func (p point) String() string {
+	return "("+strconv.Itoa(p.x)+","+strconv.Itoa(p.y)+")"
+}
+
+func (p point) calcDistance() int {
+	return p.x+p.y
+}
+
+func newPoint(x,y int) point { return point{x,y} }
+
+func parsePointsToVectors(in string) []point {
+	splitted := strings.Split(in, ",")
+	out := make([]point, len(splitted)+1)
+	out[0] = newPoint(0,0)
+	for i := 0; i < len(splitted); i++ {
+		toParse := splitted[i]
+		dir := rune(toParse[0])
+		val,_ := strconv.Atoi(toParse[1:])
+		
+		x,y := out[i].x,out[i].y
+		switch dir {
+		case 'U': y += val
+		case 'D': y -= val
+		case 'L': x-=val
+		case 'R': x+=val
+		}
+		out[i+1] = newPoint(x,y)
+	}
+	return out
+}
+
+type segment struct {
+	start point
+	end point
+}
+
+func newSegment(start, end point) segment { return segment{start,end} }
+
+func (s segment) intersection() *point {
+	return nil
+}
--- adventOfCode2019/day3/day3_test.go
+++ adventOfCode2019/day3/day3_test.go
@@ -1,11 +1,10 @@
-package day2
+package day3
 
 import (
-	"strconv"
-	"strings"
 	"testing"
 	"github.com/stretchr/testify/assert"
 )
+
 // https://adventofcode.com/2019/day/3
 func TestPointParser(t *testing.T) {
 	testCases := []struct {
@@ -39,41 +38,7 @@ func TestDistance(t *testing.T) {
 		})
 	}
 }
+
 func TestTodo(t *testing.T) {
 	assert.Fail(t, "todo")
-}
-
-type point struct {
-	x int
-	y int
-}
-func (p point) String() string {
-	return "("+strconv.Itoa(p.x)+","+strconv.Itoa(p.y)+")"
-}
-
-func (p point) calcDistance() int {
-	return p.x+p.y
-}
-
-func newPoint(x,y int) point { return point{x,y} }
-
-func parsePointsToVectors(in string) []point {
-	splitted := strings.Split(in, ",")
-	out := make([]point, len(splitted)+1)
-	out[0] = newPoint(0,0)
-	for i := 0; i < len(splitted); i++ {
-		toParse := splitted[i]
-		dir := rune(toParse[0])
-		val,_ := strconv.Atoi(toParse[1:])
-		
-		x,y := out[i].x,out[i].y
-		switch dir {
-		case 'U': y += val
-		case 'D': y -= val
-		case 'L': x-=val
-		case 'R': x+=val
-		}
-		out[i+1] = newPoint(x,y)
-	}
-	return out
 }
