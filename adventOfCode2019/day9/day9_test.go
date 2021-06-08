f5510bb762be81a6e0c4e3f579be11a41809c757
Kamil <kamil.figon@gmail.com>
2021-06-08 20:52:14 +0200

    pending impl p1

--- adventOfCode2019/day9/day9_test.go
+++ adventOfCode2019/day9/day9_test.go
@@ -1,11 +1,14 @@
 package day9
 
 import (
+	"aoc2019/intcode"
 	"io"
 	"os"
 	"strconv"
+	"strings"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -19,10 +22,10 @@ func readFile(t *testing.T) []int {
 	content, err := io.ReadAll(file)
 	require.NoError(t, err)
 
-	strContent := string(content)
+	splitted := strings.Split(string(content),",")
 	out := make([]int, 0)
-	for i := 0; i < len(strContent); i++ {
-		v, err := strconv.Atoi(string(strContent[i]))
+	for i := 0; i < len(splitted); i++ {
+		v, err := strconv.Atoi(string(splitted[i]))
 		require.NoError(t, err)
 		
 		out = append(out, v)
@@ -30,6 +33,9 @@ func readFile(t *testing.T) []int {
 	return out
 	
 }
-func Test(t *testing.T) {
-	t.Fail()
+func TestPart1(t *testing.T) {
+	file := readFile(t)
+	c := intcode.NewComputer(file)
+	c.Calc()
+	assert.Equal(t, 123, c.GetOutput())
 }
