8685bf6eb9067549626de1f172c0318d67376a69
Kamil <kamil.figon@gmail.com>
2021-06-09 22:10:11 +0200

    utils

--- adventOfCode2019/day10/day10_test.go
+++ adventOfCode2019/day10/day10_test.go
@@ -1,6 +1,7 @@
 package day10
 
 import (
+	"fmt"
 	"io"
 	"os"
 	"strings"
@@ -20,14 +21,68 @@ func readFile(t *testing.T) []string {
 	content, err := io.ReadAll(file)
 	require.NoError(t, err)
 
-	splitted := strings.Split(string(content),"\r\n")
+	return split(string(content), "\r\n")
+}
+
+func split(data, separator string) []string {
+	splitted := strings.Split(data, separator)
 	out := []string{}
 	for i := 0; i < len(splitted); i++ {
 		out = append(out, splitted[i])
 	}
 	return out
-	
 }
+
+func TestUtils(t *testing.T) {
+	data := `.#..#.
+......
+#####.
+....#.
+...##.`
+	s := spaceMap(split(data,"\n"))
+	assert.Equal(t, 5, s.rows())
+	assert.Equal(t, 6, s.cols())
+}
+
+func TestCharAt(t *testing.T) {
+	testCases := []struct {
+		x int
+		y int
+		exp rune
+	}{
+		{0,0,'.'},
+		{1,0,'#'},
+		{2,0,'.'},
+		{3,0,'.'},
+		{4,0,'#'},
+		{0,1,'.'},
+		{0,2,'#'},
+		{4,3,'#'},
+	}
+	data := `.#..#
+.....
+#####
+....#`
+	s := spaceMap(split(data,"\n"))
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("%v-%v",tc.x,tc.y), func(t *testing.T) {
+			assert.Equal(t, tc.exp, s.charAt(tc.x, tc.y))
+		})
+	}
+}
+
 func TestPart1(t *testing.T) {
 	assert.Fail(t, "todo")
 }
+
+func isAsteroid(c rune) bool { return c =='#' }
+type spaceMap []string
+
+func (s spaceMap) charAt(x,y int) rune {
+	return rune(s[y][x])
+}
+
+func (s spaceMap) rows() int { return len(s) }
+func (s spaceMap) cols() int { return len(s[0]) }
+
+
