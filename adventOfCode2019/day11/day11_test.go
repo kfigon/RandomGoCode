25f8c21176523e012120af29d8506ecfee6eda3d
Kamil <kamil.figon@gmail.com>
2021-06-21 18:34:09 +0200

    example done

--- adventOfCode2019/day11/day11_test.go
+++ adventOfCode2019/day11/day11_test.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -30,8 +31,113 @@ func readFile(t *testing.T) []int {
 	return out	
 }
 
-func TestAsd(t *testing.T) {
-	t.Fail()
-	// comp := intcode.NewComputer(readFile(t))
-	// finished := comp.SingleInstruction()
+func TestRobot(t *testing.T) {
+	controller := newController()
+
+	controller.process(1,0)
+	controller.process(0,0)
+	controller.process(1,0)
+	controller.process(1,0)
+	
+	controller.process(0,1)
+	controller.process(1,0)
+	controller.process(1,0)
+
+	assert.Equal(t, 6, len(controller.grid.m))
+}
+
+const (
+	DIRECTION_UP = iota
+	DIRECTION_DOWN
+	DIRECTION_LEFT
+	DIRECTION_RIGHT
+)
+
+type position struct { x,y int }
+
+const (
+	COLOR_BLACK = iota
+	COLOR_WHITE
+)
+type grid struct {
+	m map[position]int
+}
+
+func newGrid() *grid {
+	return &grid{
+		m: map[position]int{},
+	}
+}
+
+type robot struct {
+	position
+	direction int
+}
+
+func newRobot() *robot {
+	return &robot{}
+}
+
+func (this *robot) left() {
+	switch this.direction {
+	case DIRECTION_UP: 
+		this.direction = DIRECTION_LEFT
+		this.x--
+	case DIRECTION_DOWN: 
+		this.direction = DIRECTION_RIGHT
+		this.x++
+	case DIRECTION_LEFT:
+		this.direction = DIRECTION_DOWN
+		this.y--
+	case DIRECTION_RIGHT:
+		this.direction = DIRECTION_UP
+		this.y++
+	}
+}
+
+func (this *robot) right() {
+	switch this.direction {
+	case DIRECTION_UP: 
+		this.direction = DIRECTION_RIGHT
+		this.x++
+	case DIRECTION_DOWN: 
+		this.direction = DIRECTION_LEFT
+		this.x--
+	case DIRECTION_LEFT:
+		this.direction = DIRECTION_UP
+		this.y++
+	case DIRECTION_RIGHT:
+		this.direction = DIRECTION_DOWN
+		this.y--
+	}
+}
+
+type robotController struct {
+	robot *robot
+	grid *grid
+}
+
+func newController() *robotController {
+	return &robotController{
+		robot: newRobot(),
+		grid: newGrid(),
+	}
+}
+
+const (
+	MOVE_LEFT = iota
+	MOVE_RIGHT
+)
+func (this *robotController) process(paintInstruction, moveInstruction int) {
+	this.grid.m[this.robot.position] = paintInstruction
+
+	if moveInstruction == MOVE_LEFT {
+		this.robot.left()
+	} else if moveInstruction == MOVE_RIGHT {
+		this.robot.right()
+	}
+}
+
+func (this *robotController) currentColor() int {
+	return this.grid.m[this.robot.position]
 }
