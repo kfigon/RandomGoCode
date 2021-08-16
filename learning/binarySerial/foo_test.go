6c984d45a9536b97be53a207f0f80455782c7eef
Kamil <kamil.figon@gmail.com>
2021-08-16 22:41:59 +0200

    more binary serialization

--- learning/binarySerial/foo_test.go
+++ learning/binarySerial/foo_test.go
@@ -7,9 +7,9 @@ import (
 )
 
 type data struct {
-	foo int32
-	bar int32
-	x bool
+	Foo int32
+	Bar int32
+	X bool
 }
 
 func TestAsd(t *testing.T) {
@@ -32,5 +32,18 @@ func TestAsd(t *testing.T) {
 		if exp[i] != got[i] {
 			t.Error("Invalid data on",i)
 		}
+	}
+
+	var unmarshaled data
+	err = binary.Read(&buf, binary.BigEndian, &unmarshaled)
+	if err != nil {
+		t.Fatal("Got error", err)
+	}
+	if d != unmarshaled {
+		t.Error("Invalid body after unmarshalling", unmarshaled)
+	}
+
+	if d.Bar != unmarshaled.Bar || d.Foo != unmarshaled.Foo || d.X != unmarshaled.X {
+		t.Error("Invalid body after unmarshalling", unmarshaled)
 	}
 }
