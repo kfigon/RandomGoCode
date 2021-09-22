d15968ca6f9f30e38df318d3810666932e6dc03a
Kamil <kamil.figon@gmail.com>
2021-09-22 20:54:25 +0200

    cleanup, start parser all over

--- compilers/parser/parser.go
+++ compilers/parser/parser.go
@@ -8,12 +8,7 @@ type Grammar int
 
 const (
 	Expression Grammar = iota
-	BinaryOperator
 	Statement
-	Assignment
-	If
-	Loop
-	// Function
 )
 
 func Parse(tokens []lexer.Token) *Tree {
@@ -21,33 +16,3 @@ func Parse(tokens []lexer.Token) *Tree {
 }
 
 type Tree struct {}
-
-type BinaryNode struct {
-	Operation string
-	Left *ExpressionNode
-	Right *ExpressionNode
-}
-
-type ExpressionNode struct {
-	Assign *AssignmentNode
-	Op *BinaryNode
-}
-
-type StatementNode struct {
-	Exp *ExpressionNode
-}
-
-type AssignmentNode struct {
-	Identifier string
-	Expression *ExpressionNode
-}
-
-type IfNode struct {
-	Predicate *ExpressionNode
-	Statement *StatementNode
-}
-
-type LoopNode struct {
-	End *ExpressionNode
-	Statement *StatementNode
-}
