7dc60687a5c6700ba5bca239fa8bc4eeac8c5abb
Kamil <kamil.figon@gmail.com>
2021-09-22 20:12:39 +0200

    cli refactored

--- compilers/main.go
+++ compilers/main.go
@@ -16,16 +16,13 @@ func main() {
 	}()
 
 	fmt.Println("Welcome to my bad compiler")
-
-	filePath := flag.String("file", "", "path to file with code")
-	lex := flag.Bool("lex", false, "print lexer output")
-	flag.Parse()
-
-	if filePath == nil || *filePath == "" {
-		fmt.Println("Filepath not provided")
+	
+	cfg := parseCliArgsToConfig()
+	if err := validate(cfg); err != nil {
+		fmt.Println(err)
 		return
 	}
-	file, err := os.Open(*filePath)
+	file, err := os.Open(cfg.filePath)
 	if err != nil {
 		fmt.Println("Error when reading file:", err)
 		return
@@ -38,12 +35,39 @@ func main() {
 		return
 	}
 	fileContent := string(fileByteContent)
-	if *lex {
+	if cfg.lex {
 		tokens := lexer.Tokenize(fileContent)
 		fmt.Println("Tokens:")
 		for _, t := range tokens {
 			fmt.Println(t)
 		}
 		return
+	} else if cfg.runRepl {
+		fmt.Println("Running repl...")
+		// todo
+		return
 	}
+}
+
+type config struct {
+	filePath string
+	lex bool
+	runRepl bool
+}
+
+func parseCliArgsToConfig() config {
+	var cfg config
+	flag.StringVar(&cfg.filePath, "file", "", "path to file with code")
+	flag.BoolVar(&cfg.lex, "lex", false, "lexes file and prints lexer output")
+	flag.BoolVar(&cfg.runRepl, "repl", false, "run REPL, ignores all other params")
+	flag.Parse()
+
+	return cfg
+}
+
+func validate(c config) error {
+	if c.lex && c.filePath == "" {
+		return fmt.Errorf("filepath not provided")
+	} 
+	return nil
 }
