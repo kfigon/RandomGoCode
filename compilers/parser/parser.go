6af8bdae7fbfd12340ee6cc8d244a285c5475825
Kamil <kamil.figon@gmail.com>
2021-09-23 22:38:35 +0200

    more parser code

--- compilers/parser/parser.go
+++ compilers/parser/parser.go
@@ -12,7 +12,18 @@ const (
 )
 
 func Parse(tokens []lexer.Token) *Tree {
-	return nil
+	iter := newIterator(tokens)
+	tree := &Tree{}
+	for {
+		tok, ok := iter.next()
+		if !ok {
+			break
+		}
+		if isVarKeyword(tok) {
+		}
+	}
+
+	return tree
 }
 
 type Tree struct {
@@ -27,4 +38,20 @@ type LetStatementNode struct {
 
 type ExpressionNode struct {
 
+}
+
+func isVarKeyword(token lexer.Token) bool {
+	return token.Class == lexer.Keyword && token.Lexeme == "var"
+}
+
+func isEqualOperator(token lexer.Token) bool {
+	return token.Class == lexer.Operator && token.Lexeme == "="
+}
+
+func isSemicolon(token lexer.Token) bool {
+	return token.Class == lexer.Semicolon && token.Lexeme == ";"
+}
+
+func isNumberLiteral(token lexer.Token) bool {
+	return token.Class == lexer.Number
 }
--- compilers/parser/parser_test.go
+++ compilers/parser/parser_test.go
@@ -5,45 +5,22 @@ import (
 	"programming-lang/lexer"
 )
 
-func TestBasicMathTree(t *testing.T) {
-	t.Run("Addition", func(t *testing.T) {
-		// 1+2
-		tokens := []lexer.Token{
-			{Class: lexer.Number, Lexeme: "1"},
-			{Class: lexer.Operator, Lexeme: "+"},
-			{Class: lexer.Number, Lexeme: "2"},
-		}
-
-		tree := Parse(tokens)
-
-		if tree == nil {
-			t.Error("Invalid tree built")
-		}	
-	})
-
-	t.Run("Operation order", func(t *testing.T) {
-		// 1+2*3
-		tokens := []lexer.Token{
-			{Class: lexer.Number, Lexeme: "1"},
-			{Class: lexer.Operator, Lexeme: "+"},
-			{Class: lexer.Number, Lexeme: "2"},
-			{Class: lexer.Operator, Lexeme: "*"},
-			{Class: lexer.Number, Lexeme: "3"},
-		}
-
-		tree := Parse(tokens)
-
-		if tree == nil {
-			t.Error("Invalid tree built")
-		}	
-	})	
-}
-
 func TestVarStatement(t *testing.T) {
-	tokens := lexer.Tokenize(`var foo = 123;`)
+	tokens := lexer.Tokenize(`var foo = 123;
+	var asd = 3;`)
 	
 	tree := Parse(tokens)
-	if tree == nil {
-		t.Error("Invalid tree built")
-	}	
+	if len(tree.Statements) != 2 {
+		t.Fatal("Invalid tree built, should be 2, got",len(tree.Statements))
+	}
+	if tree.Statements[0].Name == nil || 
+		*tree.Statements[0].Name != "foo" || 
+		tree.Statements[0].Value == nil {
+		t.Error("Invalid first statement", tree.Statements[0])
+	}
+	if tree.Statements[1].Name == nil || 
+		*tree.Statements[1].Name != "asd" || 
+		tree.Statements[1].Value == nil {
+		t.Error("Invalid second statement", tree.Statements[1])
+	}
 }
