9817054c29593fb3307d1c2f5dbd76d15ec6b6da
Kamil <kamil.figon@gmail.com>
2022-01-05 17:16:50 +0100

    d9 done!

--- adventOfCode2021/d9/d9_test.go
+++ adventOfCode2021/d9/d9_test.go
@@ -1,7 +1,9 @@
 package d9
 
 import (
+	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 	"testing"
@@ -29,9 +31,51 @@ func TestP1(t *testing.T) {
 		t.Error("Exp", 585, "got", got)
 	}
 }
-func TestP2(t *testing.T) {
-	t.Fatal("todo")
+func TestExampleP2Basins(t *testing.T) {
+	data := parse(exampleData, "\n")
+	tdt := []struct {
+		p pair
+		exp int
+	}{
+		{pair{r:0, c: 1},3},
+		{pair{r:0, c: 9},9},
+		{pair{r:2, c: 2},14},
+		{pair{r:4, c: 6},9},
+	}
+	for i, tc := range tdt {
+		t.Run(fmt.Sprintf("%v", i), func(t *testing.T) {
+			v, ok := basin(data, tc.p)
+			if !ok {
+				t.Error("Expected basin, not found")
+			}
+			if v != tc.exp {
+				t.Error("Exp",tc.exp,"got",v)
+			}
+		})
+	}
 }
+
+func TestExampleP2(t *testing.T) {
+	data := parse(exampleData, "\n")
+	got := solveP2(data)
+	if got != 1134 {
+		t.Error("Exp", 1134, "got", got)
+	}
+}
+
+func TestP2(t *testing.T) {
+	d,err := os.ReadFile("data.txt")
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	got := solveP2(parse(string(d), "\r\n"))
+	exp := 827904
+	if got != exp {
+		t.Error("Exp", exp, "got", got)
+	}
+}
+
 func parse(data string, sep string) [][]int {
 	lines := strings.Split(data,sep)
 	out := [][]int{}
@@ -46,45 +90,136 @@ func parse(data string, sep string) [][]int {
 	return out
 }
 
-func solveP1(data [][]int) int {
-	neighbours := func(row,col int) []int {
-		out := []int{}
-		type pair struct {
-			r int
-			c int
-		}
+type pair struct {
+	r int
+	c int
+}
 
-		candidates := []pair {
-				{row-1, col},
-			{row, col-1}, {row, col+1},
-				{row+1, col},
-		}
-		for _, c := range candidates {
-			if c.r < 0 || c.r >= len(data) || c.c < 0 || c.c >= len(data[0]) {
-				continue
-			}
-			out = append(out, data[c.r][c.c])
-		}
-		return out
+func inBounds(data [][]int, c pair) bool {
+	return c.r >= 0 && c.r < len(data) && c.c >= 0 && c.c < len(data[0])
+}
+
+func neighbours(data [][]int, p pair) []pair {
+	out := []pair{}
+
+	row := p.r
+	col := p.c
+	candidates := []pair {
+			{row-1, col},
+		{row, col-1}, {row, col+1},
+			{row+1, col},
 	}
 
-	sum := 0
+	for _, c := range candidates {
+		if inBounds(data, c) {
+			out = append(out, c)
+		}
+	}
+	return out
+}
+
+func isLowPoint(data [][]int, p pair) bool {
+	current := data[p.r][p.c]
+	nei := neighbours(data, p)
+
+	for _, v := range nei {
+		value := data[v.r][v.c]
+		if current >= value {
+			return false
+		}
+	}
+	return true
+}
+
+func findLowPoints(data [][]int) []pair {
+	points := []pair{}
 	for r := 0; r < len(data); r++ {
 		for c := 0; c < len(data[0]); c++ {
-			current := data[r][c]
-			nei := neighbours(r,c)
-
-			allOk := true
-			for _, v := range nei {
-				if current >= v {
-					allOk = false
-					break
-				}
-			}
-			if allOk {
-				sum+=current+1
+			v := pair{r:r,c:c}
+			if isLowPoint(data, v) {
+				points = append(points, v)
 			}
 		}
 	}
+	return points
+}
+
+func solveP1(data [][]int) int {
+	lowPoints := findLowPoints(data)
+
+	sum := 0
+	for _, v := range lowPoints {
+		current := data[v.r][v.c]
+		sum += current+1
+	}
 	return sum
+}
+
+func solveP2(data [][]int) int {
+	basinSizes := []int{}
+	lowPoints := findLowPoints(data)
+	for _, v := range lowPoints {
+		if v, ok := basin(data, v); ok {
+			basinSizes = append(basinSizes, v)
+		} 
+	}
+
+	sort.Ints(basinSizes)
+	threeBiggest := basinSizes[len(basinSizes)-3:]
+	result := 1
+	for _, v := range threeBiggest {
+		result *= v
+	}
+	return result
+}
+
+func basin(data[][]int, current pair) (int, bool) {
+	basinCandidates := newQueue()
+
+	addNeighbours := func(p pair) {
+		for _, v := range neighbours(data, p) {
+			candidateValue := data[v.r][v.c]
+			currentValue := data[p.r][p.c]
+
+			if candidateValue != 9 && candidateValue > currentValue {
+				basinCandidates.enqueue(v)
+			}
+		}
+	}
+	
+	basin := map[pair]struct{}{}
+	basin[current] = struct{}{}
+
+	// recursive also can work
+	addNeighbours(current)
+	for !basinCandidates.empty() {
+		el := basinCandidates.dequeue()
+		basin[el]=struct{}{}
+		addNeighbours(el)
+	}
+	return len(basin),true
+}
+
+type queue struct {
+	d []pair
+}
+
+func newQueue() *queue {
+	return &queue{
+		d: []pair{},
+	}
+}
+
+func (s *queue) enqueue(p pair) {
+	s.d = append(s.d, p)
+}
+
+func (s *queue) empty() bool {
+	return len(s.d) == 0
+}
+
+func (s *queue) dequeue() pair {
+	fist := s.d[0]
+	s.d = s.d[1:]
+	return fist
 }
