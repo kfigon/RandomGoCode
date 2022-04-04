52fcd4145b8b54672ae6fa954324e5cb61b3e109
Kamil <kamil.figon@gmail.com>
2022-04-04 21:49:16 +0200

    infix in progress

--- compilers/parser/expressions.go
+++ compilers/parser/expressions.go
@@ -14,15 +14,16 @@ type ExpressionNode interface {
 type IntegerLiteralExpression struct {
 	Value int
 }
+
 func (ile *IntegerLiteralExpression) TokenLiteral() string {
 	return strconv.Itoa(ile.Value)
 }
 func (ile *IntegerLiteralExpression) evaluateExpression() {}
 
-
 type IdentifierExpression struct {
 	Name string
 }
+
 func (ide *IdentifierExpression) TokenLiteral() string {
 	return ide.Name
 }
@@ -30,8 +31,9 @@ func (ide *IdentifierExpression) evaluateExpression() {}
 
 type PrefixExpression struct {
 	Operator string
-	Right ExpressionNode
+	Right    ExpressionNode
 }
+
 func (p *PrefixExpression) TokenLiteral() string {
 	return p.Operator
 }
@@ -39,8 +41,8 @@ func (p *PrefixExpression) evaluateExpression() {}
 
 type InfixExpressionNode struct {
 	Operator string
-	Left ExpressionNode
-	Right ExpressionNode
+	Left     ExpressionNode
+	Right    ExpressionNode
 }
 
 func (i *InfixExpressionNode) TokenLiteral() string {
@@ -51,12 +53,12 @@ func (i *InfixExpressionNode) evaluateExpression() {}
 const (
 	_ int = iota
 	LOWEST
-	EQUALS // ==
+	EQUALS      // ==
 	LESSGREATER // > or <
-	SUM // +
-	PRODUCT // *
-	PREFIX // -X or !X
-	CALL // myFunction(X)
+	SUM         // +
+	PRODUCT     // *
+	PREFIX      // -X or !X
+	CALL        // myFunction(X)
 )
 
 func (p *parser) parseExpression(predescense int) ExpressionNode {
@@ -68,29 +70,65 @@ func (p *parser) parseExpression(predescense int) ExpressionNode {
 	}
 	case isNumberLiteral(tok): return p.parseIntegerLiteralExpression()
 	case isIdentifier(tok): return p.parseIdentifierExpression()
-	case bang(tok) || minus(tok): return p.parsePrefixExpression()
 	}
 
+	if bang(tok) || minus(tok) {
+		return p.parsePrefixExpression() 
+	}
+
+	// // prefix
+	// var left ExpressionNode
+	// if bang(tok) || minus(tok) {
+	// 	left = p.parsePrefixExpression() 
+	// }
+
+	// // infix
+	// for !isSemicolon(p.nextToken) && predescense < tokensPredescense(p.nextToken) {
+
+	// 	if equals(p.nextToken) || notEquals(p.nextToken) || lessThan(p.nextToken) || 
+	// 		lessEqThan(p.nextToken) || greaterEqThan(p.nextToken) || 
+	// 		greaterThan(p.nextToken) || plus(p.nextToken) || minus(p.nextToken) || 
+	// 		product(p.nextToken) || divide(p.nextToken) {
+			
+	// 		p.advanceToken()
+	// 		left = p.parseInfixExpression(left)
+	// 	} else {
+	// 		return left
+	// 	}
+	// }
+
+	// return left
 	p.addError(fmt.Errorf("expression error - syntax error %q", tok.Lexeme))
 	return nil
 }
 
-func predescense(tok lexer.Token) int {
+func tokensPredescense(tok lexer.Token) int {
 	switch {
-	case equals(tok): return EQUALS
-	case notEquals(tok): return EQUALS
-	
-	case lessThan(tok): return LESSGREATER
-	case lessEqThan(tok): return LESSGREATER
-	case greaterEqThan(tok): return LESSGREATER
-	case greaterThan(tok): return LESSGREATER
+	case equals(tok):
+		return EQUALS
+	case notEquals(tok):
+		return EQUALS
 
-	case plus(tok): return SUM
-	case minus(tok): return SUM
-	
-	case product(tok): return PRODUCT
-	case divide(tok): return PRODUCT
-	default: return LOWEST
+	case lessThan(tok):
+		return LESSGREATER
+	case lessEqThan(tok):
+		return LESSGREATER
+	case greaterEqThan(tok):
+		return LESSGREATER
+	case greaterThan(tok):
+		return LESSGREATER
+
+	case plus(tok):
+		return SUM
+	case minus(tok):
+		return SUM
+
+	case product(tok):
+		return PRODUCT
+	case divide(tok):
+		return PRODUCT
+	default:
+		return LOWEST
 	}
 }
 
@@ -110,7 +148,7 @@ func (p *parser) parseIntegerLiteralExpression() ExpressionNode {
 		return nil
 	}
 	p.advanceToken()
-	return &IntegerLiteralExpression{Value: v}		
+	return &IntegerLiteralExpression{Value: v}
 }
 
 func (p *parser) parseIdentifierExpression() ExpressionNode {
@@ -136,9 +174,9 @@ func (p *parser) parsePrefixExpression() ExpressionNode {
 func (p *parser) parseInfixExpression(left ExpressionNode) ExpressionNode {
 	out := &InfixExpressionNode{
 		Operator: p.currentToken.Lexeme,
-		Left: left,
+		Left:     left,
 	}
-	pred := predescense(p.currentToken)
+	pred := tokensPredescense(p.currentToken)
 	p.advanceToken()
 	out.Right = p.parseExpression(pred)
 	return out
