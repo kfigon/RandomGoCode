dd12360c18ac9c466b295aa424b6967abd3bb08f
Kamil <kamil.figon@gmail.com>
2022-08-10 21:11:58 +0200

    RESP command parsing

--- redis-clone/server.go
+++ redis-clone/server.go
@@ -26,7 +26,48 @@ func startServer() {
 	scanner := bufio.NewScanner(conn)
 	for scanner.Scan() {
 		data := scanner.Text()
-		fmt.Println(data)
+		handleCommand(data)
 	}
 	fmt.Println("that's all folks")
+}
+
+func handleCommand(data string) {
+	fmt.Println(data)
+	cmd := newCmd(&data)
+
+	if err := cmd.validate(); err != nil {
+		fmt.Println("Invalid command:", err)
+		return
+	}
+
+	if cmd.isStringCmd() {
+		fmt.Println("got STRING")
+	}
+}
+
+func newCmd(data *string) *command {
+	c := command(*data)
+	return &c
+}
+
+type command string
+func (c *command) validate() error {
+	if len(*c) < 3 {
+		return fmt.Errorf("too short")
+	} else if !c.isStringCmd() && (*c)[0] != '*' && (*c)[0] != '$' {
+		return fmt.Errorf("invalid first character: %v", (*c)[0])
+	} else if c.termination() != `\r\n` {
+		return fmt.Errorf("invalid termination: %q", c.termination())
+	}
+	return nil
+}
+
+func (c *command) isStringCmd() bool {
+	return (*c)[0] == '+'
+}
+
+func (c *command) termination() string {
+	sub := string(*c)
+	ln := len(sub)-2
+	return sub[ln:]
 }
