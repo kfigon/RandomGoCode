285e98c0a2e8a8bceb1fe80f733de4b49905985f
kfigon <kamil.figon@gmail.com>
2022-09-25 12:04:55 +0200

    bencode parser boiler

---
+++ bencodeParser/main_test.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+func Test(t *testing.T) {
+	assert.Fail(t, "todo")
+}
---
+++ bencodeParser/readme.md
@@ -0,0 +1,5 @@
+## Bencode parser
+https://en.wikipedia.org/wiki/Bencode
+
+credits to https://xmonader.github.io/nimdays/day02_bencode.html
+
