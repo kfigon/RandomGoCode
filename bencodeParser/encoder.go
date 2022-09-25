ef72e446b22d730a30189b1eeba1a325f0581c19
kfigon <kamil.figon@gmail.com>
2022-09-25 13:02:42 +0200

    becode encoder done

--- bencodeParser/encoder.go
+++ bencodeParser/encoder.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func encodeStr(in string) string {
 	return fmt.Sprintf("%v:%v", len(in), in)
@@ -13,16 +16,36 @@ func encodeInt(in int) string {
 func encodeList(in []any) string {
 	out := "l"
 	for _, v := range in {
-		switch v.(type){
-		case int:
-		case string:
-		case []any:
-		case map[string]any:
-		}
+		out += encodeAnyType(v)
 	}
 	return out+"e"
 }
 
 func encodeDict(in map[string]any) string {
+	out := "d"
+	type entry struct {
+		key string
+		val any
+	}
+	listed := []entry{}
+	for k, v := range in {
+		listed = append(listed, entry{key: k, val: v})
+	}
+	sort.Slice(listed, func(i, j int) bool {
+		return listed[i].key < listed[j].key
+	})
+	for _,v := range listed {
+		out += encodeStr(v.key) + encodeAnyType(v.val)
+	}
+	return out + "e"
+}
+
+func encodeAnyType(v any) string {
+	switch val := v.(type){
+		case int: return encodeInt(val)
+		case string: return encodeStr(val)
+		case []any: return encodeList(val)
+		case map[string]any: return encodeDict(val)
+	}
 	return ""
 }
--- bencodeParser/main_test.go
+++ bencodeParser/main_test.go
@@ -19,7 +19,7 @@ func TestEncoder(t *testing.T) {
 		assert.Equal(t, "li2ei58ee", encodeList([]any{2,58}))
 		assert.Equal(t, "l4:spami42ee", encodeList([]any{"spam",42}))
 		
-		assert.Equal(t, "l2:hili1ei2eed3:fooi58e4:barz3:asdei42ee", encodeList([]any{
+		assert.Equal(t, "l2:hili1ei2eed4:barz3:asd3:fooi58eei42ee", encodeList([]any{
 			"hi",
 			[]any{1,2},
 			map[string]any{"foo": 58, "barz": "asd"},
@@ -29,6 +29,10 @@ func TestEncoder(t *testing.T) {
 
 	t.Run("dict", func(t *testing.T) {
 		assert.Equal(t, "d3:bar4:spam3:fooi42ee", encodeDict(map[string]any{"bar": "spam", "foo": 42}))
-		assert.Equal(t, "d3:bar3:asd3:foo4:spami42ee", encodeDict(map[string]any{"bar": "asd", "foo": []any{"spam",42}}))
+		assert.Equal(t, "d3:bar4:spam3:fooi42ee", encodeDict(map[string]any{"foo": 42, "bar": "spam"}))
+		assert.Equal(t, "d3:bar3:asd3:fool4:spami42eee", encodeDict(map[string]any{
+			"bar": "asd", 
+			"foo": []any{"spam",42},
+		}))
 	})
 }
