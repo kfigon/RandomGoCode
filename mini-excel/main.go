fca6d14bef2c2de2b752b7af71a41d95c3616cc4
Kamil <kamil.figon@gmail.com>
2022-10-02 00:06:37 +0200

    cli arg

--- mini-excel/main.go
+++ mini-excel/main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
 	fmt.Println("asd")
+	if len(os.Args) < 2 {
+		fmt.Println("no file provided")
+		return
+	}
+	path := os.Args[1]
+	fmt.Println(path)
 }
