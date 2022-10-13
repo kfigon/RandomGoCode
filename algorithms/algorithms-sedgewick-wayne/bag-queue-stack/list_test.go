0835cc0c1155b65354479aaee78d2b9a318ac803
kfigon <kamil.figon@gmail.com>
2022-10-13 17:56:48 +0200

    basic singly linked list

--- algorithms/algorithms-sedgewick-wayne/bag-queue-stack/list_test.go
+++ algorithms/algorithms-sedgewick-wayne/bag-queue-stack/list_test.go
@@ -2,9 +2,70 @@ package main
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 
 func TestSinglyLinkedList(t *testing.T) {
-	t.Fatal("todo")
+	t.Run("empty", func(t *testing.T) {
+		s := &singleLinkedList{}
+		assert.Empty(t, s.getElements())
+	})
+
+	t.Run("single", func(t *testing.T) {
+		s := &singleLinkedList{}
+		s.add(2)
+		assert.Equal(t, []int{2}, s.getElements())
+	})
+
+	t.Run("many", func(t *testing.T) {
+		s := &singleLinkedList{}
+		s.add(2)
+		s.add(3)
+		s.add(4)
+		s.add(5)
+		assert.Equal(t, []int{2,3,4,5}, s.getElements())
+	})
+}
+
+type listNode struct {
+	val int
+	next *listNode
+}
+
+func newListNode(val int) *listNode {
+	return &listNode{
+		val: val,
+	}
+}
+
+type singleLinkedList struct {
+	root *listNode
+}
+
+func (s *singleLinkedList) add(val int) {
+	newNode := newListNode(val)
+	if s.root == nil {
+		s.root = newNode
+		return
+	}
+
+	last := s.root
+	for last.next != nil {
+		last = last.next
+	}
+	last.next = newNode
+}
+
+func (s *singleLinkedList) getElements() []int {
+	out := []int{}
+
+	ptr := s.root
+	for ptr != nil {
+		out = append(out, ptr.val)
+		ptr = ptr.next
+	}
+
+	return out
 }
