abf22c10546df871283959de5b1a6b2e87f4fcff
Kamil <kamil.figon@gmail.com>
2022-10-16 22:50:19 +0200

    api for union find

--- algorithms/algorithms-sedgewick-wayne/union-find/quickfind_test.go
+++
@@ -1,7 +0,0 @@
-package unionfind
-
-import "testing"
-
-func TestQuickFind(t *testing.T) {
-	t.Fatal("todo")
-}
--- algorithms/algorithms-sedgewick-wayne/union-find/unionfind_test.go
+++ algorithms/algorithms-sedgewick-wayne/union-find/unionfind_test.go
@@ -5,3 +5,16 @@ import "testing"
 func TestUnionFind(t *testing.T) {
 	t.Fatal("todo")
 }
+
+type unionFind struct{}
+
+func (u *unionFind) union(p,q int){}
+func (u *unionFind) find(p int) int{
+	return -1
+}
+func (u *unionFind) connected(p,q int) bool{
+	return false
+}
+func (u *unionFind) count(p int) int{
+	return -1
+}
