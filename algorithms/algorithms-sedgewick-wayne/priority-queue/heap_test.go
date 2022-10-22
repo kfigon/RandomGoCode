6d4a6cc7a76c1a2785083ae14a6a627187d8f6f3
kfigon <kamil.figon@gmail.com>
2022-10-22 10:43:03 +0200

    api for heap

---
+++ algorithms/algorithms-sedgewick-wayne/priority-queue/heap_test.go
@@ -0,0 +1,41 @@
+package priorityqueue
+
+import "testing"
+
+// heap (binary heap) - structure where each key is guaranteed to be larger
+// to the 2 children. Array or binary tree(+ link to parrent)
+// # BinaryHeap is kind of binarySearchTree (2 nodes)
+// # maxBinaryHeap - parent is always larger than children nodes
+// # minBinaryHeap - parent is always smaller than childen nodes
+// heap is always balanced (binary tree is not, can be tall)
+
+func TestHeap(t *testing.T) {
+	t.Fatal("todo")	
+}
+
+type heap[T any] interface {
+	insert(pair[int, T])
+	max() (pair[int, T], bool)
+	delMax() (pair[int, T], bool)
+}
+
+type heapEl[T any] pair[int, T]
+type arrayHeap[T any] struct {
+	tab []heapEl[T]
+}
+
+func newArrayHeap[T any]() *arrayHeap[T] {
+	return &arrayHeap[T]{tab: []heapEl[T]{}}
+}
+
+func (a *arrayHeap[T]) insert(v heapEl[T]) {
+
+}
+
+func (a *arrayHeap[T]) max() (heapEl[T], bool) {
+	return heapEl[T]{}, false
+}
+
+func (a *arrayHeap[T]) delMax() (heapEl[T], bool) {
+	return heapEl[T]{}, false
+}
--- algorithms/algorithms-sedgewick-wayne/priority-queue/priorityqueue_test.go
+++ algorithms/algorithms-sedgewick-wayne/priority-queue/priorityqueue_test.go
@@ -14,30 +14,20 @@ import "testing"
 // unordered arr	  1			N
 // heap			  	 logN      logN
 
-// heap (binary heap) - structure where each key is guaranteed to be larger
-// to the 2 children. Array or binary tree(+ link to parrent)
-// # BinaryHeap is kind of binarySearchTree (2 nodes)
-// # maxBinaryHeap - parent is always larger than children nodes
-// # minBinaryHeap - parent is always smaller than childen nodes
-// heap is always balanced (binary tree is not, can be tall)
 func TestPriorityQueue(t *testing.T) {
 	t.Fatal("todo")
 }
 
-
-type comparable[T any] interface {
-	less(a, b T) bool
-}
 
 type pair[T any, V any] struct {
 	a T
 	b V
 }
 
-type priorityQueue[T comparable[T]] interface {
-	insert(comparable[T])
-	max() comparable[T]
-	delMax() comparable[T]
+type priorityQueue[T any] interface {
+	insert(pair[int, T])
+	max() pair[int, T]
+	delMax() pair[int, T]
 	size() int
 	empty() bool
 }
