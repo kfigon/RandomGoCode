2c748a1aa4b271024ebebf375217088add6bf133
Kamil <kamil.figon@gmail.com>
2023-01-24 23:49:41 +0100

    repl

--- lisp/main.go
+++ lisp/main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("hello")
+	if len(os.Args) == 1 {
+		interpreterMode()
+	} else if len(os.Args) == 2 {
+		fileMode(os.Args[1])
+	} else {
+		fmt.Println("Invalid number of arguments")
+	}
+}
+
+func fileMode(fileName string) {
+	b, err := os.ReadFile(fileName)
+	if err != nil {
+		fmt.Printf("Cant open file %v: %v\n", fileName, err)
+		return
+	}
+	t, err := lex(string(b))
+	if err != nil {
+		fmt.Println("Got error:", err)
+		return
+	}
+	fmt.Println(t)
+}
+
+func interpreterMode() {
+	fmt.Println("Welcome to lisp interpreter")
+	fmt.Println("type 'quit' to exit")
+	for true {
+		var line string
+		fmt.Print("> ")
+		fmt.Scanln(&line)
+		
+		if line == "quit" {
+			fmt.Println("Bye")
+			return
+		} else if line != "" {
+			fmt.Println(line)
+		}
+	}
 }
