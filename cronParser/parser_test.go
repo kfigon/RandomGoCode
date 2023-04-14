8d8f7480422a9b976647e0f3b2a35dc7f934b1c6
Kamil <kamil.figon@gmail.com>
2023-04-14 21:15:38 +0200

    better test descriptions

--- cronParser/parser_test.go
+++ cronParser/parser_test.go
@@ -10,59 +10,67 @@ import (
 func TestParser(t *testing.T) {
 	testCases := []struct {
 		input string
+		desc string
 		exp []int
 	}{
 		{
 			input: `5`,
+			desc: "just number",
 			exp: []int{5},
 		},
 		{
 			input: `*`,
+			desc: "wildcard",
 			exp: []int{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59},
 		},
 		{
 			input: `5-10`,
+			desc: "range",
 			exp: []int{5,6,7,8,9,10},
 		},
 		{
 			input: `5,6,7,8,9,10`,
+			desc: "enumerated",
 			exp: []int{5,6,7,8,9,10},
 		},
 		{
 			input: `1,2,3,10,11,12`,
+			desc: "enumerated with skip",
 			exp: []int{1,2,3,10,11,12},
 		},
 		{
 			input: `5-10,20-25`,
+			desc: "ranges enumerated",
 			exp: []int{5,6,7,8,9,10,20,21,22,23,24,25},
 		},
 		{
-			input: `5-10`,
-			exp: []int{5,6,7,8,9,10},
-		},
-		{
 			input: `*/10`,
+			desc: "wildcard with step",
 			exp: []int{0,10,20,30,40,50},
 		},
 		{
 			input: `0-20/10`,
+			desc: "range with step",
 			exp: []int{0,10,20},
 		},
 		{
 			input: `0-22/10`,
+			desc: "non overlapping range with step",
 			exp: []int{0,10,20},
 		},
 		{
 			input: `1,2,3,4,5,6,7,8,9,10/2`,
+			desc: "enumeration with step",
 			exp: []int{2,4,6,8,10},
 		},
 		{
 			input: `5,20-30/5`,
+			desc: "enumeration and ranges with step",
 			exp: []int{5,20,25,30},
 		},
 	}
 	for _, tC := range testCases {
-		t.Run(tC.input, func(t *testing.T) {
+		t.Run(tC.desc, func(t *testing.T) {
 			got, err := tokenize(tC.input)
 			require.NoError(t, err, "unexpected lexer error")
 
