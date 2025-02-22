1fc0817e3f76d6b2bd9aec6aa61e53b49117d5c7
kfigon <kamil.figon@gmail.com>
2025-02-22 22:08:15 +0100

    basic lock free chat

---
+++ chat/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+// nc localhost 8000
+func main() {
+    fmt.Println("hello")
+    s := newServer()
+    fmt.Println(s.run(8000))
+}
+
+type clientChan chan<- string
+
+type server struct {
+    text chan string
+    userEnter chan clientChan // channel of channels
+    userLeft chan clientChan
+}
+
+func newServer() *server {
+    return &server{
+        text: make(chan string),
+        userEnter: make(chan clientChan),
+        userLeft: make(chan clientChan),
+    }
+}
+
+func (s *server) run(port int) error {
+    conn, err := net.Listen("tcp4", fmt.Sprintf(":%d", port))
+    if err != nil {
+        return err
+    }
+    defer conn.Close()
+
+    go s.broadcaster()
+
+    for {
+        c, err := conn.Accept()
+        if err != nil {
+            fmt.Println("error accepting connection", err)
+            continue
+        }
+        go s.handleConn(c)
+    }
+}
+
+func (s *server) broadcaster() {
+    clients := make(map[clientChan]struct{}) // all connected clients
+    for {
+        select {
+        case msg := <-s.text:
+            // Broadcast incoming message to all clients channels
+            for client := range clients {
+                client <- msg
+            }
+
+        case cli := <-s.userEnter:
+            clients[cli] = struct{}{}
+
+        case cli := <-s.userLeft:
+            delete(clients, cli)
+            close(cli)
+        }
+    }
+}
+
+func (s *server) handleConn(con net.Conn) {
+    userChan := make(chan string)
+    go func() {
+        // write all other msgs to this chan
+        for msg := range userChan {
+            fmt.Fprintln(con, msg)
+        }
+    }()
+
+    s.userEnter <- userChan
+    addr := con.RemoteAddr()
+    
+    fmt.Println("client connected", addr)
+    
+    defer con.Close()
+    scanner := bufio.NewScanner(con)
+    for scanner.Scan() {
+        s.text <- fmt.Sprintf("%s says %s\n", addr, scanner.Text())
+    }
+    fmt.Println("closed", addr)
+    s.userLeft <- userChan
+}
