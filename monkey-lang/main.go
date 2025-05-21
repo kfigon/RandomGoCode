6b8349e1aede696de0aa2a019d01369a7112791f
kfigon <kamil.figon@gmail.com>
2025-05-21 21:12:42 +0200

    .

--- monkey-lang/main.go
+++ monkey-lang/main.go
@@ -2,22 +2,8 @@ package main
 
 import (
 	"fmt"
-	"iter"
-	"slices"
 )
 
 func main() {
-	fmt.Println("hello")
-
-	it := slices.Values([]int{1,2,3,4,5})
-	next, stop := iter.Pull(it)
-	defer stop()
-	for {
-		v, ok := next()
-		if !ok {
-			break
-		}
-		fmt.Println(v)
-	}
-	fmt.Println("all done")
+	fmt.Println("asdf")
 }
